7f72ccaaf60d71de7c4c6c29ec495bb8307a22aa
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-02 17:05:19 +0100

    feat: new component

---
+++ src/components/templates/Main.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Main = styled.main`
+  height: 100%;
+  padding: 1rem 2rem;
+`
+
+export default Main
---
+++ src/components/templates/Page.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Page = styled.div`
+  height: 85vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.dark};
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font-size: 1.4rem;
+  overflow-y: auto;
+`
+
+export default Page
