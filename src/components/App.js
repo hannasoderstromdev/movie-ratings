1d8dd29980169530c3eb1dd37deb7613c0a72de2
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-02 17:04:41 +0100

    feat: add routing, Home and New page

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,38 @@
 import React from 'react'
+import { Router } from '@reach/router'
+import styled from 'styled-components'
 
 import Theme from './Theme'
 import Normalize from './Normalize'
 
+import Header from 'components/molecules/Header'
+import Navigation from 'components/molecules/Navigation'
+
 import Home from './pages/Home'
+import New from './pages/New'
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  justify-content: space-between;
+`
 
 const App = () => (
   <div>
     <Theme>
-      <>
+      <Wrapper>
         <Normalize />
-        <Home />
-      </>
+        <Header />
+
+        <Router>
+          <Home path="/" />
+          <New path="/new" />
+        </Router>
+
+        <Navigation />
+      </Wrapper>
     </Theme>
   </div>
 )
