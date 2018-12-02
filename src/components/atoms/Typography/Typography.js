840fa18df7c176dc7f9a05fe59b6273446bd98c4
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-02 17:04:51 +0100

    feat: new components

---
+++ src/components/atoms/Typography/Typography.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.textPrimary};
+`
+
+export const H2 = styled.h2`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.textPrimary};
+`
+
+export const Text = styled.span`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.textPrimary};
+`
+
+export const TextDark = styled.span`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.textSecondary};
+`
