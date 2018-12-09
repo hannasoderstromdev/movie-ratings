44d803918c43441158e5b068260853bc6bdf5eea
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-09 12:21:00 +0100

    feat: add onMouseOver and onMouseOut

--- src/components/molecules/Star/Star.js
+++ src/components/molecules/Star/Star.js
@@ -7,14 +7,17 @@ import Icon from '../../atoms/Icon'
 const StarButton = styled.button`
   border: none;
   padding: 0;
+  outline: none;
 `
 
-const Star = ({ onClick, isSelected }) => (
+const Star = ({ onClick, isSelected, onMouseOver, onMouseOut }) => (
   <StarButton onClick={onClick}>
     <Icon
       icon={['fas', 'star']}
       iconSize="1.5rem"
       color={isSelected ? '#FEDC9B' : '#666'}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
     />
   </StarButton>
 )
@@ -22,6 +25,8 @@ const Star = ({ onClick, isSelected }) => (
 Star.propTypes = {
   isSelected: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
+  onMouseOver: PropTypes.func.isRequired,
+  onMouseOut: PropTypes.func.isRequired,
 }
 
 export default Star
