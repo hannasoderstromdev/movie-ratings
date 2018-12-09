1b5521e399966fb311f934e2cedad6df2c06eb07
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-09 11:13:43 +0100

    feat: new component

--- src/components/molecules/Rating/Rating.js
+++ src/components/molecules/Rating/Rating.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Rating = ({ rating }) => <div>{rating}</div>
+import Star from '../Star'
+
+const RatingStyle = styled.div`
+  display: flex;
+`
+
+const Rating = ({ rating, increase, decrease }) => {
+  const stars = []
+  for (let i = 0; i < rating; i++) {
+    stars.push(<Star key={i} isSelected onClick={decrease} />)
+  }
+
+  for (let i = 0; i < 5 - rating; i++) {
+    stars.push(<Star key={stars.length + i} onClick={increase} />)
+  }
+  return <RatingStyle>{stars}</RatingStyle>
+}
 
 Rating.propTypes = {
+  decrease: PropTypes.func.isRequired,
+  increase: PropTypes.func.isRequired,
   rating: PropTypes.number.isRequired,
 }
 
---
+++ src/components/molecules/Rating/Rating.md
@@ -0,0 +1,9 @@
+#
+
+```js
+<Rating 
+  rating={4}
+  increase={() => console.log('increase')}
+  decrease={() => console.log('decrease')}
+/>
+```
