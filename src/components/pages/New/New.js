3a30bb4ca0216983c982d128cf0ec0e94068c9f8
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-09 10:39:58 +0100

    feat: content for page New

--- src/components/pages/New/New.js
+++ src/components/pages/New/New.js
@@ -1,7 +1,45 @@
 import React from 'react'
 
-import Page from 'components/templates/Page'
+import { H1 } from 'components/atoms/Typography'
 
-const New = () => <Page>New</Page>
+import Page from 'components/templates/Page'
+import Main from 'components/templates/Main'
+
+import SearchField from 'components/molecules/SearchField'
+
+class New extends React.Component {
+  state = {
+    search: '',
+  }
+
+  doOnChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  doOnSubmit = e => {
+    e.preventDefault()
+    console.log('search for movie ', this.state.search)
+  }
+
+  render() {
+    return (
+      <Page>
+        <Main>
+          <H1>New</H1>
+          <form>
+            <SearchField
+              name="search"
+              onChange={this.doOnChange}
+              placeholder="Search for Movie in Open Movie Database"
+              onSubmit={this.doOnSubmit}
+            />
+          </form>
+          <div>Search results</div>
+          <div>Set rating: </div>
+        </Main>
+      </Page>
+    )
+  }
+}
 
 export default New
