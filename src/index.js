6823d96cc47b15336db38bf1145979e16fba2ae9
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-09 10:38:38 +0100

    feat: add font-awesome and the first icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import App from './components/App'
+
+// Add all icons used here
+library.add(faTimes)
 
 ReactDOM.render(<App />, document.getElementById('root'))
