f6d76d73c07c91c10459a154d3b2cf9cac748e95
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-09 16:52:48 +0100

    refactor: MoviesList

--- src/components/molecules/MoviesList/MoviesList.js
+++ src/components/molecules/MoviesList/MoviesList.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 import Movie from 'components/molecules/Movie'
 
-const MoviesList = ({ movies }) => {
-  return movies.map(movie => <Movie key={movie.id} {...movie} />)
-}
+const MoviesList = ({ movies }) =>
+  movies.map(movie => <Movie key={movie.imdbID} {...movie} />)
 
 export default MoviesList
