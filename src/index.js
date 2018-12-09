68a40c4c65f77563dfb5dcf2d0a2c3520958f147
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-09 11:13:34 +0100

    feat: add faStar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import App from './components/App'
 
 // Add all icons used here
-library.add(faTimes)
+library.add(faStar, faTimes)
 
 ReactDOM.render(<App />, document.getElementById('root'))
