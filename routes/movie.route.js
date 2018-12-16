b79e846166700e5ad10c382f3a8f06f7e575da56
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-16 12:00:41 +0100

    fix: change route for create to /create

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -4,7 +4,7 @@ const router = new Router()
 const movieController = require('../controllers/movie.controller')
 
 router.get('/', movieController.getAll)
-router.post('/', movieController.create)
+router.post('/create', movieController.create)
 router.get('/:movieId', movieController.getById)
 router.put('/:movieId', movieController.updateById)
 router.delete('/:movieId', movieController.deleteById)
