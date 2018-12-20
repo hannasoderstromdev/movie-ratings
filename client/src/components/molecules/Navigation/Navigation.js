bed95429226e28e58fe16f6ee04ba9cdf0900f9f
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-20 12:44:40 +0100

    fix: change to react-router

--- client/src/components/molecules/Navigation/Navigation.js
+++ client/src/components/molecules/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from '@reach/router'
+import { Link } from 'react-router-dom'
 
 const Nav = styled.nav`
   background-color: ${({ theme }) => theme.colors.black};
