04ad5b07242b0d9a934ecc510f1510e5ae57c059
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-20 16:58:26 +0100

    feat: new search-actions

---
+++ client/src/actions/search/search.actions.js
@@ -0,0 +1,38 @@
+import searchService from 'services/search/search.service'
+import { alertErrorAction } from '../alerts/alerts.actions'
+
+export const SEARCH_FOR_MOVIE_TITLE = 'SEARCH_FOR_MOVIE_TITLE'
+export const SEARCH_FOR_MOVIE_TITLE_SUCCESS = 'SEARCH_FOR_MOVIE_TITLE_SUCCESS'
+export const SEARCH_FOR_MOVIE_TITLE_FAILURE = 'SEARCH_FOR_MOVIE_TITLE_FAILURE'
+
+const searchForMovieTitleAction = () => ({
+  type: SEARCH_FOR_MOVIE_TITLE,
+})
+
+const searchForMovieTitleSuccessAction = movie => ({
+  type: SEARCH_FOR_MOVIE_TITLE_SUCCESS,
+  payload: { movie },
+})
+
+const searchForMovieTitleFailureAction = error => ({
+  type: SEARCH_FOR_MOVIE_TITLE_FAILURE,
+  payload: { error },
+})
+
+export const searchOMDB = movieTitle => async dispatch => {
+  dispatch(searchForMovieTitleAction())
+
+  try {
+    const result = await searchService.search(movieTitle)
+
+    if (result.status === 'error') {
+      throw Error(result.message)
+    }
+
+    dispatch(searchForMovieTitleSuccessAction(result))
+  } catch (error) {
+    console.log('error', error.message)
+    dispatch(searchForMovieTitleFailureAction(error))
+    dispatch(alertErrorAction(error.message))
+  }
+}
