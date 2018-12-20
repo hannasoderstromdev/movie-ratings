d6f3473e7e5415cb7f0f0d5834a5b7ad58893848
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-20 12:45:11 +0100

    feat: controller for searching OMDB

---
+++ controllers/search.controller.js
@@ -0,0 +1,50 @@
+const request = require('request')
+const keys = require('../config/keys')
+
+module.exports = {
+  /**
+   * Search for movies in the Open Movie Database
+   */
+
+  search: function(req, res, next) {
+    const response = request(
+      `http://www.omdbapi.com/?apikey=${keys.OMDB_API_KEY}&t=${
+        req.query.movieTitle
+      }`,
+      function(error, response, body) {
+        if (error) {
+          next(error)
+        } else {
+          const data = JSON.parse(response.body)
+
+          res.json({
+            status: 'success',
+            message: 'movie found',
+            data: {
+              title: data.Title,
+              year: data.Year,
+              released: data.Released,
+              runtime: data.Runtime,
+              genre: data.Genre,
+              director: data.Director,
+              writer: data.Writer,
+              actors: data.Actors,
+              plot: data.Plot,
+              language: data.Language,
+              country: data.Country,
+              awards: data.Awards,
+              poster: data.Poster,
+              ratings: data.Ratings,
+              metascore: data.Metascore,
+              imdbRating: data.imdbRating,
+              imdbVotes: data.imdbVotes,
+              imdbID: data.imdbID,
+              production: data.Production,
+              website: data.Website,
+            },
+          })
+        }
+      },
+    )
+  },
+}
