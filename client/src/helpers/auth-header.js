1561edf0420a7524c8f15f8f4a875b583bdf3a98
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-20 10:20:02 +0100

    chore: move file

--- client/src/helpers/auth-header.js
+++
@@ -1,6 +0,0 @@
-export function authHeader() {
-  const user = JSON.parse(localStorage.getItem('user'))
-  return (user && user.token) 
-    ? { 'Authorization': 'Bearer ' + user.token } 
-    : {}
-}
