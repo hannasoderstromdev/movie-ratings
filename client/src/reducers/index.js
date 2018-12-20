fd17caa80c49ec14367b8b0abc700710cc844cd7
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-20 10:20:12 +0100

    feat: add user-reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
 
 import alerts from './alerts/alerts.reducer'
-import authentication from './authentication/authentication.reducer'
+import user from './user/user.reducer'
 import movies from './movies/movies.reducer'
 
 const rootReducer = combineReducers({
   alerts,
-  authentication,
+  user,
   movies,
 })
 
