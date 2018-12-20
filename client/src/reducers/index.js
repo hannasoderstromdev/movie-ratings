55e83e8ae7ab3799d07eb9c5f9825a930ae3deb4
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-20 17:00:22 +0100

    feat: add search-reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import alerts from './alerts/alerts.reducer'
 import user from './user/user.reducer'
 import movies from './movies/movies.reducer'
+import search from './search/search.reducer'
 
 const rootReducer = combineReducers({
   alerts,
   user,
   movies,
+  search,
 })
 
 export default rootReducer
