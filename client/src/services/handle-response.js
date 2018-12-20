32b67db71dcaf434e87ca43e223216629b8d7619
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-20 10:20:23 +0100

    feat: handle response errors

---
+++ client/src/services/handle-response.js
@@ -0,0 +1,22 @@
+import userService from './user/user.service'
+
+/**
+ * Handles response from fetch
+ * return Promise.reject on error
+ * return data if ok
+ */
+
+const handleResponse = async response => {
+  const text = await response.text()
+  const data = await JSON.parse(text)
+  if (!response.ok) {
+    if (response.status === 401) {
+      userService.logout()
+    }
+
+    const error = (await data.message) || (await response.statusText)
+    return Promise.reject(error)
+  }
+  return data
+}
+export default handleResponse
