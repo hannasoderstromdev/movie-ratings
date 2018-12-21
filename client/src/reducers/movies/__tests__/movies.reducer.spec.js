c8384986a7cd388bcbb3f6a43d706c4f2c76c807
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 20:26:47 +0100

    test: for movies.reducer

---
+++ client/src/reducers/movies/__tests__/movies.reducer.spec.js
@@ -0,0 +1,37 @@
+import {
+  GET_ALL_MOVIES,
+  GET_ALL_MOVIES_SUCCESS,
+  GET_ALL_MOVIES_FAILURE,
+} from 'actions/movies/movies.actions'
+
+import reducer from '../movies.reducer'
+
+describe('Reducers/Movies', () => {
+  it(`handles ${GET_ALL_MOVIES}`, () => {
+    const action = {
+      type: GET_ALL_MOVIES,
+    }
+    const expectedState = { error: null, loading: true, movies: [] }
+    expect(reducer({}, action)).toEqual(expectedState)
+  })
+
+  it(`handles ${GET_ALL_MOVIES_SUCCESS}`, () => {
+    const movies = []
+    const action = {
+      type: GET_ALL_MOVIES_SUCCESS,
+      payload: { movies },
+    }
+    const expectedState = { error: null, loading: false, movies: [] }
+    expect(reducer({}, action)).toEqual(expectedState)
+  })
+
+  it(`handles ${GET_ALL_MOVIES_FAILURE}`, () => {
+    const error = 'Error message'
+    const action = {
+      type: GET_ALL_MOVIES_FAILURE,
+      payload: { error },
+    }
+    const expectedState = { error: 'Error message', loading: false, movies: [] }
+    expect(reducer({}, action)).toEqual(expectedState)
+  })
+})
