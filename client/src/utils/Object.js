928a3da320660e2f6185a2963aca8d684d57a4be
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 16:02:27 +0100

    feat: update method for checking if object is empty

--- client/src/utils/Object.js
+++ client/src/utils/Object.js
@@ -1,3 +1,12 @@
 export function isEmptyObject(obj) {
+  if (obj === null) return true
+  if (obj === undefined) return true
+
+  // for (const key in obj) {
+  //   if (obj.hasOwnProperty(key)) {
+  //     return false
+  //   }
+  //   return true
+  // }
   return !Object.keys(obj).length > 0
 }
