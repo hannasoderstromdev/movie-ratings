373b78dcd6f853794e1a3645ef0acc872dff0005
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 16:01:50 +0100

    feat: update authHeader

--- client/src/services/auth-header.js
+++ client/src/services/auth-header.js
@@ -1,4 +1,10 @@
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'))
-  return user && user.token ? { Authorization: 'Bearer ' + user.token } : {}
+  return user && user.token
+    ? {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-access-token': user.token,
+      }
+    : {}
 }
