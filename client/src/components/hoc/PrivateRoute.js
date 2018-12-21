14716534a26bab4d4e4723a6c667241173787ffb
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 16:00:17 +0100

    chore: fix proptype

--- client/src/components/hoc/PrivateRoute.js
+++ client/src/components/hoc/PrivateRoute.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    localStorage.getItem('user') ? (
-      <Component {...props} /> 
-    ) : (
-    <Redirect to={{ pathname: '/login', state: { from : props.location }}} />
-    )
-  )} />
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem('user') ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: '/login', state: { from: props.location } }}
+        />
+      )
+    }
+  />
 )
 
 PrivateRoute.propTypes = {
-  component: PropTypes.node.isRequired,
+  component: PropTypes.any.isRequired,
 }
 
 export default PrivateRoute
