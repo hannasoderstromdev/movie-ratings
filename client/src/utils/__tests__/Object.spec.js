b8be2b0f0fc17f86f0edc9c52dec97ec8f4538f5
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 16:02:41 +0100

    test: change text

--- client/src/utils/__tests__/Object.spec.js
+++ client/src/utils/__tests__/Object.spec.js
@@ -2,13 +2,13 @@ import { isEmptyObject } from '../Object'
 
 describe('Utils/Object', () => {
   describe('isEmptyObject', () => {
-    it('returns true if Object has no keys', () => {
+    it('returns true if Object is empty', () => {
       const myObject = {}
 
       expect(isEmptyObject(myObject)).toEqual(true)
     })
 
-    it('returns false if Object has keys', () => {
+    it('returns false if Object has props', () => {
       const myObject = {
         myProp: 'value',
       }
