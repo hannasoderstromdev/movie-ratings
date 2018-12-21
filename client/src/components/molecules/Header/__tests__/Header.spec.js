489edf6cc200993cec90909866a5142821a41be5
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 22:59:11 +0100

    test: for Header

---
+++ client/src/components/molecules/Header/__tests__/Header.spec.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+
+import Header from '..'
+import Theme from '../../../Theme'
+
+describe('<Header />', () => {
+  it('renders', () => {
+    const { getByText } = render(
+      <Theme>
+        <Header />
+      </Theme>,
+    )
+    expect(getByText('Movie Ratings').tagName).toBe('HEADER')
+  })
+})
