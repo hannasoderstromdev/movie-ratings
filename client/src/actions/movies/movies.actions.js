043fa2bd5da83c0982e14aba39e999ef90b52b85
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 15:59:42 +0100

    feat: add actions for creating movie

--- client/src/actions/movies/movies.actions.js
+++ client/src/actions/movies/movies.actions.js
@@ -5,7 +5,11 @@ export const GET_ALL_MOVIES = 'GET_ALL_MOVIES'
 export const GET_ALL_MOVIES_SUCCESS = 'GET_ALL_MOVIES_SUCCESS'
 export const GET_ALL_MOVIES_FAILURE = 'GET_ALL_MOVIES_SUCCESS'
 
-const getAllMoviesAction = movies => ({
+export const CREATE_MOVIE = 'CREATE_MOVIE'
+export const CREATE_MOVIE_SUCCESS = 'CREATE_MOVIE_SUCCESS'
+export const CREATE_MOVIE_FAILURE = 'CREATE_MOVIE_FAILURE'
+
+const getAllMoviesAction = () => ({
   type: GET_ALL_MOVIES,
 })
 
@@ -19,6 +23,20 @@ const getAllMoviesFailureAction = error => ({
   payload: { error },
 })
 
+const createMovieAction = () => ({
+  type: CREATE_MOVIE,
+})
+
+const createMovieSuccessAction = movie => ({
+  type: CREATE_MOVIE_SUCCESS,
+  payload: { movie },
+})
+
+const createMovieFailureAction = error => ({
+  type: CREATE_MOVIE_FAILURE,
+  payload: { error },
+})
+
 export const getAllMovies = () => async dispatch => {
   dispatch(getAllMoviesAction())
 
@@ -27,6 +45,18 @@ export const getAllMovies = () => async dispatch => {
     dispatch(getAllMoviesSuccessAction(movies))
   } catch (error) {
     dispatch(getAllMoviesFailureAction(error))
+    dispatch(alertErrorAction(error))
+  }
+}
+
+export const createMovie = movie => async dispatch => {
+  dispatch(createMovieAction())
+
+  try {
+    const createdMovie = await moviesService.create(movie)
+    dispatch(createMovieSuccessAction(createdMovie))
+  } catch (error) {
+    dispatch(createMovieFailureAction(error))
     dispatch(alertErrorAction(error))
   }
 }
