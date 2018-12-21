6e1f6fb3e0542d7faa636c33a2ffa42b969fc7c3
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-22 00:04:15 +0100

    test: for Navigation

---
+++ client/src/components/molecules/Navigation/__tests__/Navigation.spec.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navigation from '..'
+import Theme from '../../../Theme'
+
+describe('<Navigation />', () => {
+  it('renders', () => {
+    const props = {}
+    const { getByText } = render(
+      <Theme>
+        <Router>
+          <Navigation {...props} />
+        </Router>
+      </Theme>,
+    )
+    expect(getByText('Home').tagName).toBe('A')
+    expect(getByText('New').tagName).toBe('A')
+    expect(getByText('Login').tagName).toBe('A')
+    // TODO: find a way to check for the settings icon
+  })
+})
