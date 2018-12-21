f3f54fb193670603f773086e8b45edb50f3d6ebf
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-21 14:18:46 +0100

    feat: add icon

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faTimes, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 
 import App from './components/App'
 
 // Add all icons used here
-library.add(faStar, faTimes)
+library.add(faStar, faTimes, faEllipsisV)
 
 ReactDOM.render(<App />, document.getElementById('root'))
