27a248b9c2169c1b9d440c2e941741caecf16691
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-22 11:00:58 +0100

    test: for SearchField

---
+++ client/src/components/molecules/SearchField/__tests__/SearchField.spec.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+
+import SearchField from '../SearchField'
+import Theme from '../../../Theme'
+
+describe('Components/Molecules/SearchField', () => {
+  const onChange = jest.fn()
+  const props = {
+    onChange,
+    name: 'searchfield',
+  }
+  const { getByTestId, getByText, debug } = render(
+    <Theme>
+      <SearchField {...props} />
+    </Theme>,
+  )
+
+  it('renders', () => {
+    expect(getByTestId('searchfield').tagName).toBe('INPUT')
+    expect(getByTestId('searchfield').type).toBe('search')
+    expect(getByText('Search')).toBeDefined()
+    debug()
+  })
+
+  it('handles change', () => {
+    fireEvent.change(getByTestId('searchfield'), { target: { value: 'a' } })
+    expect(onChange).toHaveBeenCalledTimes(1)
+  })
+})
