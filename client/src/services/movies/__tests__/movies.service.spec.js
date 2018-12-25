95ab47a0415eb30d82fd2e87996e8927a847a596
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-25 13:44:30 +0100

    test: for movies and search service

---
+++ client/src/services/movies/__tests__/movies.service.spec.js
@@ -0,0 +1,118 @@
+import fetchMock from 'fetch-mock'
+import moviesService from '../movies.service'
+
+describe('Services/Movies', () => {
+  fetchMock.config.overwriteRoutes = true
+
+  describe('getAll', () => {
+    it('handles successful request', async () => {
+      const data = [
+        {
+          id: 'mid01',
+          title: 'The Title',
+          year: '1998',
+          runtime: '136 min',
+          director: 'Someone',
+          actors: 'Me and Someone',
+          poster: 'http://www.site.com/poster01.jpg',
+          imdbID: 'tt001',
+          rating: 4,
+        },
+        {
+          id: 'mid02',
+          title: 'The Title 2',
+          year: '1999',
+          runtime: '130 min',
+          director: 'Someone',
+          actors: 'Me and Someone',
+          poster: 'http://www.site.com/poster02.jpg',
+          imdbID: 'tt002',
+          rating: 2,
+        },
+      ]
+
+      fetchMock.mock('/movies', {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data,
+        }),
+      })
+
+      const result = await moviesService.getAll()
+
+      const expectedResult = { data }
+
+      expect(result).toEqual(expectedResult)
+    })
+
+    it('handles failed request', async () => {
+      const data = null
+
+      fetchMock.mock('/movies', {
+        status: 403,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data,
+          message: 'Unauthorized',
+        }),
+      })
+
+      try {
+        await moviesService.getAll()
+      } catch (error) {
+        const expectedResult = 'Unauthorized'
+        expect(error).toEqual(expectedResult)
+      }
+    })
+  })
+
+  describe('create', () => {
+    it('handles successful request', async () => {
+      fetchMock.mock('/movies/create', {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          status: 'success',
+          message: 'Movie added successfully',
+          data: null,
+        }),
+      })
+      const movie = {}
+      const result = await moviesService.create(movie)
+      const expectedResult = {
+        data: null,
+        message: 'Movie added successfully',
+        status: 'success',
+      }
+
+      expect(result).toEqual(expectedResult)
+    })
+    it('handles failed request', async () => {
+      fetchMock.mock('/movies/create', {
+        status: 403,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: 'Unauthorized',
+        }),
+      })
+
+      try {
+        const movie = {}
+        await moviesService.create(movie)
+      } catch (error) {
+        const expectedResult = 'Unauthorized'
+
+        expect(error).toEqual(expectedResult)
+      }
+    })
+  })
+})
---
+++ client/src/services/search/__tests__/search.service.spec.js
@@ -0,0 +1,54 @@
+import fetchMock from 'fetch-mock'
+import searchService from '../search.service'
+
+describe('Services/SearchService', () => {
+  fetchMock.config.overwriteRoutes = true
+
+  describe('search', () => {
+    beforeEach(() => {
+      localStorage.setItem('user', JSON.stringify({ token: 'fake token' }))
+    })
+
+    afterEach(() => {
+      localStorage.clear()
+    })
+
+    it('handles successful request', async () => {
+      const data = {}
+
+      fetchMock.mock('/search/?movieTitle=test', {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data,
+        }),
+      })
+      const response = await searchService.search('test')
+      expect(response).toEqual({ data: {} })
+    })
+
+    it('handles failed request', async () => {
+      localStorage.setItem('user', JSON.stringify({ token: 'fake token' }))
+
+      const data = {}
+
+      fetchMock.mock('/search/?movieTitle=test', {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data,
+        }),
+      })
+
+      try {
+        await searchService.search('test')
+      } catch (error) {
+        expect(error).toEqual('Internal Server Error')
+      }
+    })
+  })
+})
