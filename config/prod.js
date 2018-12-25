5432c2dd9be855e78c1748627d61095faf396f9e
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-25 14:19:32 +0100

    fix: replace . with ,

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,8 @@
 modules.export = {
-  env: 'production',
-  mongoURI: process.env.MONGO_URI,
-  JWTSecret: process.env.JWT_SECRET,
+  ENV: 'production',
+  ROOT_URL: process.env.ROOT_URL,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
   OMDB_API_KEY: process.env.OMDB_API_KEY,
-  port: process.env.PORT,
+  PORT: process.env.PORT,
 }
