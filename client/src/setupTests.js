03205f134a7a9037ee1130684e11aaf712fd2796
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-25 13:41:31 +0100

    feat: add localstorage-mock

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,4 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faStar, faTimes, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import 'jest-localstorage-mock'
 
 library.add(faStar, faTimes, faEllipsisV)
