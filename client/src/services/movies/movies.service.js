4c105defc840a3e01d6ff776195db46deb62dcc0
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-25 13:44:20 +0100

    feat: add headers manually

--- client/src/services/movies/movies.service.js
+++ client/src/services/movies/movies.service.js
@@ -3,33 +3,37 @@ import authHeader from '../auth-header'
 import handleResponse from '../handle-response'
 
 const getAll = async () => {
+  const user = JSON.parse(localStorage.getItem('user'))
+
   const requestOptions = {
     method: 'GET',
-    headers: authHeader(),
+    headers: {
+      Accept: 'application/json',
+      'x-access-token': user.token,
+    },
   }
 
   const response = await fetch('/movies', requestOptions)
-  const data = await handleResponse(response)
-
-  console.log('getAll', data)
+  const { data } = await handleResponse(response)
 
   return data
 }
 
 const create = async movie => {
+  const user = JSON.parse(localStorage.getItem('user'))
+
   const requestOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      ...authHeader(),
+      Accept: 'application/json',
+      'x-access-token': user.token,
     },
     body: JSON.stringify({ ...movie }),
   }
 
-  const response = await fetch('/movies', requestOptions)
+  const response = await fetch('/movies/create', requestOptions)
   const data = await handleResponse(response)
-
-  console.log('create', data)
 
   return data
 }
