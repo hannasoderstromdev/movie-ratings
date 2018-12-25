699079288b67070140ba0e76f8fb2e32b2543d77
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-25 13:44:07 +0100

    test: update

---
+++ client/src/services/__tests__/handle-response.spec.js
@@ -0,0 +1,54 @@
+import fetchMock from 'fetch-mock'
+import handleResponse from '../handle-response'
+
+fetchMock.config.overwriteRoutes = true
+
+describe('Services/handle-response', () => {
+  it('handles valid response', async () => {
+    fetchMock.mock('/fake', {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: 'success',
+        message: 'success',
+        data: 'fake',
+      }),
+    })
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const response = await fetch('/fake', options)
+    handleResponse(response)
+  })
+
+  it('handles failed response', async () => {
+    try {
+      fetchMock.mock('/fake', {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          status: 'error',
+          message: 'Server error',
+          data: null,
+        }),
+      })
+      const options = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      const response = await fetch('/fake', options)
+      await handleResponse(response)
+    } catch (error) {
+      expect(error).toEqual('Server error')
+    }
+  })
+})
