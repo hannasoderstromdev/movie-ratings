215ab2bfc1e3be78234057e433519cc71cce83a8
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 14:00:05 +0100

    feat: update design for Alerts

--- client/src/components/molecules/Alert/Alert.js
+++ client/src/components/molecules/Alert/Alert.js
@@ -5,19 +5,19 @@ import { connect } from 'react-redux'
 
 import { alertClearAction } from 'actions/alerts/alerts.actions'
 
+import { Text } from 'components/atoms/Typography'
 import Icon from 'components/atoms/Icon'
 import Button from 'components/atoms/Button'
 
 const AlertWrapper = styled.div`
-  height: 5rem;
+  height: 4rem;
   width: 100%;
-  position: fixed;
-  bottom: 9vh;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 2rem;
   font-size: 1.6rem;
+  position: absolute;
+  bottom: 6rem;
   color: ${({ theme }) => theme.colors.textPrimary};
   background-color: ${({ type, theme }) =>
     type === 'success' && theme.colors.success};
@@ -34,16 +34,54 @@ const AlertIcon = styled(Icon)`
   color: ${({ type, theme }) => type === 'alert' && theme.colors.alertLight};
 `
 
+const IconWrapper = styled.div`
+  height: 4rem;
+  width: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ type, theme }) =>
+    type === 'success' && theme.colors.successDark};
+  background-color: ${({ type, theme }) =>
+    type === 'danger' && theme.colors.dangerDark};
+  background-color: ${({ type, theme }) =>
+    type === 'alert' && theme.colors.alertDark};
+
+  svg {
+    color: ${({ type, theme }) => type === 'success' && theme.colors.success};
+    color: ${({ type, theme }) => type === 'danger' && theme.colors.danger};
+    color: ${({ type, theme }) => type === 'alert' && theme.colors.alert};
+  }
+`
+
+const TextWrapper = styled(Text)`
+  margin: 0 1rem;
+  flex: 1;
+`
+
 const Alert = ({ display, type, message, alertClearAction }) => {
   return display ? (
     <AlertWrapper type={type}>
-      {message}
-      <Button secondary onClick={alertClearAction}>
+      <IconWrapper type={type}>
+        {type === 'success' && (
+          <Icon icon={['fas', 'check-circle']} iconsize="18px" />
+        )}
+        {type === 'danger' && (
+          <Icon icon={['fas', 'exclamation-triangle']} iconsize="18px" />
+        )}
+        {type === 'alert' && (
+          <Icon icon={['fas', 'exclamation-circle']} iconsize="18px" />
+        )}
+      </IconWrapper>
+
+      <TextWrapper>{message}</TextWrapper>
+
+      <Button thirdiary onClick={alertClearAction}>
         <AlertIcon
           icon={['fas', 'times']}
           type={type}
           iconsize="16px"
-          color="#fff"
+          color="rgba(0,0,0, .25)"
         />
       </Button>
     </AlertWrapper>
