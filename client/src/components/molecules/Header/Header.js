1cbb9b8d6bea34df555af97183a331cd18a9dcf5
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 14:00:47 +0100

    style: change height

--- client/src/components/molecules/Header/Header.js
+++ client/src/components/molecules/Header/Header.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const HeaderStyle = styled.header`
   width: 100%;
-  height: 5rem;
+  height: 8vh;
   display: flex;
   align-items: center;
   justify-content: center;
