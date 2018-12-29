8678bc214534a9ed6defd3a9dba2365ac451029a
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 14:07:37 +0100

    fix: put everything in try/catch

--- client/src/services/search/search.service.js
+++ client/src/services/search/search.service.js
@@ -1,24 +1,29 @@
 import handleResponse from '../handle-response'
 
 const search = async movieTitle => {
-  const user = JSON.parse(localStorage.getItem('user'))
 
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'x-access-token': user.token,
-    },
+  try {
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'x-access-token': user.token,
+      },
+    }
+
+    const response = await fetch(
+      `/search/?movieTitle=${movieTitle}`,
+      requestOptions,
+    )
+
+    const data = await handleResponse(response)
+
+    return data
+  } catch (error) {
+    throw new Error(error)
   }
-
-  const response = await fetch(
-    `/search/?movieTitle=${movieTitle}`,
-    requestOptions,
-  )
-
-  const data = await handleResponse(response)
-
-  return data
 }
 
 const searchService = { search }
