5c95ba7e1c33a0a592fd28c229a65fbca67e53c6
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 14:07:53 +0100

    fix: put everything in try/catch

--- client/src/services/movies/movies.service.js
+++ client/src/services/movies/movies.service.js
@@ -1,41 +1,50 @@
-import authHeader from '../auth-header'
+// import authHeader from '../auth-header'
 
 import handleResponse from '../handle-response'
 
 const getAll = async () => {
-  const user = JSON.parse(localStorage.getItem('user'))
+  const user = await JSON.parse(localStorage.getItem('user'))
 
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'x-access-token': user.token,
-    },
+  if (user) {
+    try {
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'x-access-token': user.token,
+        },
+      }
+
+      const response = await fetch('/movies', requestOptions)
+      const { data } = await handleResponse(response)
+
+      return data
+    } catch (error) {
+      throw new Error(error)
+    }
   }
-
-  const response = await fetch('/movies', requestOptions)
-  const { data } = await handleResponse(response)
-
-  return data
 }
 
 const create = async movie => {
-  const user = JSON.parse(localStorage.getItem('user'))
+  try {
+    const user = JSON.parse(localStorage.getItem('user'))
 
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'x-access-token': user.token,
-    },
-    body: JSON.stringify({ ...movie }),
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'x-access-token': user.token,
+      },
+      body: JSON.stringify({ ...movie }),
+    }
+
+    const response = await fetch('/movies/create', requestOptions)
+    const data = await handleResponse(response)
+    return data
+  } catch (error) {
+    return error
   }
-
-  const response = await fetch('/movies/create', requestOptions)
-  const data = await handleResponse(response)
-
-  return data
 }
 
 const moviesService = { getAll, create }
