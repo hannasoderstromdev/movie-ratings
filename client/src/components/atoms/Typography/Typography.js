1efb3c1ec8eff8d5d4485cf57bdf098ffd54c64a
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 13:59:46 +0100

    feat: set font-family from theme

--- client/src/components/atoms/Typography/Typography.js
+++ client/src/components/atoms/Typography/Typography.js
@@ -1,29 +1,29 @@
 import styled from 'styled-components'
 
 export const H1 = styled.h1`
-  font-family: 'Open Sans', sans-serif;
+  font-family: ${({ theme }) => theme.fonts.primary};
   font-size: 1.6rem;
-  font-weight: 500;
+  font-weight: ${({ theme }) => theme.weight.light};
   color: ${({ theme }) => theme.colors.textPrimary};
-  margin: 1rem 0 2rem;
+  margin: 2rem 0;
 `
 
 export const H2 = styled.h2`
-  font-family: 'Open Sans', sans-serif;
+  font-family: ${({ theme }) => theme.fonts.primary};
   font-size: 1.4rem;
-  font-weight: 500;
+  font-weight: ${({ theme }) => theme.weight.regular};
   color: ${({ theme }) => theme.colors.primary};
   margin: 0 0 1rem;
 `
 
 export const Text = styled.span`
-  font-family: 'Open Sans', sans-serif;
+  font-family: ${({ theme }) => theme.fonts.primary};
   font-size: 1.2rem;
   color: ${({ theme }) => theme.colors.textPrimary};
 `
 
 export const TextDark = styled.span`
-  font-family: 'Open Sans', sans-serif;
+  font-family: ${({ theme }) => theme.fonts.primary};
   font-size: 1.2rem;
   color: ${({ theme }) => theme.colors.textSecondary};
 `
