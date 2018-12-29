6d41e8342dde812e5a1fc482920a73acd87515b4
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 14:00:36 +0100

    refactor: to subcomponents and update propTypes

--- client/src/components/molecules/Movie/Movie.js
+++ client/src/components/molecules/Movie/Movie.js
@@ -1,20 +1,16 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 import styled from 'styled-components'
 
-import { H2, Text } from 'components/atoms/Typography'
-import Rating from 'components/molecules/Rating'
+import MovieHeader from 'components/molecules/MovieHeader'
+import MoviePreview from 'components/molecules/MoviePreview'
+import MovieDetails from 'components/molecules/MovieDetails'
 
 const MovieStyle = styled.article`
   display: grid;
   grid-template-columns: 2.5fr 5fr;
   grid-gap: 1.5rem;
-`
-
-const Details = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  margin-bottom: 1rem;
 `
 
 const PosterImg = styled.img`
@@ -22,32 +18,104 @@ const PosterImg = styled.img`
   height: auto;
 `
 
-const Movie = ({ title, year, runtime, director, actors, poster, rating }) => (
-  <MovieStyle>
-    <PosterImg src={poster} alt={title} />
-    <Details>
-      <div>
-        <H2>{title}</H2>
-        <div>
-          <Text>{year}</Text> <Text>{runtime}</Text>
-        </div>
-      </div>
-      {rating ? <Rating rating={rating} /> : <div>No rating</div>}
+class Movie extends React.Component {
+  state = {
+    preview: true,
+    detailsOpen: false,
+  }
 
-      <Text>Director: {director}</Text>
-      <Text>Actors: {actors}</Text>
-    </Details>
-  </MovieStyle>
-)
+  toggleFullMovie = () => {
+    this.setState({
+      preview: false,
+    })
+  }
+
+  toggleDetails = () =>
+    this.setState(prevState => ({
+      detailsOpen: !prevState.detailsOpen,
+    }))
+
+  render() {
+    const {
+      country,
+      genre,
+      language,
+      title,
+      year,
+      runtime,
+      actors,
+      director,
+      plot,
+      poster,
+      production,
+      rating,
+      ratings,
+      released,
+      writer,
+    } = this.props
+
+    return this.state.preview ? (
+      <MoviePreview
+        toggleFullMovie={this.toggleFullMovie}
+        poster={poster}
+        title={title}
+        year={year}
+        runtime={runtime}
+        genre={genre}
+        rating={rating}
+      />
+    ) : (
+      <div>
+        <MovieStyle>
+          <PosterImg src={poster} alt={title} />
+          <MovieHeader
+            title={title}
+            year={year}
+            runtime={runtime}
+            genre={genre}
+            rating={rating}
+            poster={poster}
+          />
+        </MovieStyle>
+        <MovieDetails
+          country={country}
+          language={language}
+          ratings={ratings}
+          released={released}
+          plot={plot}
+          production={production}
+          toggle={this.toggleDetails}
+          isOpen={this.state.detailsOpen}
+          director={director}
+          actors={actors}
+          writer={writer}
+        />
+      </div>
+    )
+  }
+}
 
 Movie.propTypes = {
+  country: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   year: PropTypes.string.isRequired,
   runtime: PropTypes.string.isRequired,
   director: PropTypes.string.isRequired,
   actors: PropTypes.string.isRequired,
+  plot: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  production: PropTypes.string.isRequired,
+  ratings: PropTypes.arrayOf(
+    PropTypes.shape({
+      Source: PropTypes.string.isRequired,
+      Value: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
   rating: PropTypes.number,
+  released: PropTypes.string.isRequired,
+  writer: PropTypes.string.isRequired,
 }
 
 export default Movie
