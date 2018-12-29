4d8bd0755ae0195663bfab33449acf14c74c37a2
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 14:07:22 +0100

    chore: put everything in try/catch

--- client/src/services/user/user.service.js
+++ client/src/services/user/user.service.js
@@ -1,18 +1,17 @@
 import handleResponse from '../handle-response'
 
 const login = async (username, password) => {
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email: username, password }),
-  }
-
   try {
-    const response = await fetch(`/users/authenticate`, requestOptions)
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: username, password }),
+    }
 
+    const response = await fetch(`/users/authenticate`, requestOptions)
     const { data } = await handleResponse(response)
 
     if (data) {
@@ -21,7 +20,7 @@ const login = async (username, password) => {
 
     return data.user
   } catch (error) {
-    throw new Error(error)
+    return error
   }
 }
 
