e6612a7205f6706380576be0ac93b49234ae4ace
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-29 14:02:22 +0100

    feat: add wrapper

--- client/src/components/molecules/MoviesList/MoviesList.js
+++ client/src/components/molecules/MoviesList/MoviesList.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-import Movie from 'components/molecules/Movie'
+import MoviePreview from 'components/molecules/MoviePreview'
 
-const MoviesList = ({ movies }) =>
-  movies.map(movie => <Movie key={movie.imdbID} {...movie} />)
+const MoviesListWrapper = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+`
+
+const MoviesList = ({ movies }) => {
+  return movies.length ? (
+    <MoviesListWrapper>
+      {movies.map(movie => (
+        <MoviePreview key={movie.id} {...movie} />
+      ))}
+    </MoviesListWrapper>
+  ) : null
+}
 
 export default MoviesList
