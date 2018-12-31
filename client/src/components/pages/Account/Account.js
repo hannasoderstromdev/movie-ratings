b82515b28d16e8edaa7a2019ca0fa0f056b263b6
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-31 11:35:10 +0100

    feat: add basic content for page

--- client/src/components/pages/Account/Account.js
+++ client/src/components/pages/Account/Account.js
@@ -1,5 +1,41 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Page from 'components/templates/Page'
+import Main from 'components/templates/Main'
 
-const Account = () => <div>Content</div>
+import { H1, Text, TextDark } from 'components/atoms/Typography'
 
-export default Account
+const Account = ({ role, id, firstName, lastName, email }) => (
+  <Page>
+    <Main>
+      <H1>Account</H1>
+      <p>
+        <TextDark>Privileges: </TextDark>
+        <Text>{role}</Text>
+      </p>
+      <p>
+        <Text>
+          {firstName} {lastName}
+        </Text>
+      </p>
+      <p>
+        <Text>{email}</Text>
+      </p>
+    </Main>
+  </Page>
+)
+
+Account.propTypes = {
+  role: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+}
+
+const mapStateToProps = ({ user }) => ({
+  ...(user && user.profile && user.profile.user),
+})
+
+export default connect(mapStateToProps)(Account)
