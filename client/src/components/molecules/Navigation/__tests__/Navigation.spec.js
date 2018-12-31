b9700c79588e66f6edc921f4baf5333c1e0cedda
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-31 11:33:56 +0100

    test: update for Icon and when logged in

--- client/src/components/molecules/Navigation/__tests__/Navigation.spec.js
+++ client/src/components/molecules/Navigation/__tests__/Navigation.spec.js
@@ -1,23 +1,47 @@
 import React from 'react'
 import { render } from 'react-testing-library'
 import { BrowserRouter as Router } from 'react-router-dom'
+import configureStore from 'redux-mock-store'
 
 import Navigation from '..'
-import Theme from '../../../Theme'
+import Theme from 'components/Theme'
+import Root from 'components/Root'
+
+const mockStore = configureStore()
+let store
 
 describe('Components/Molecules/Navigation', () => {
   it('renders', () => {
-    const props = {}
-    const { getByText } = render(
-      <Theme>
-        <Router>
-          <Navigation {...props} />
-        </Router>
-      </Theme>,
+    store = mockStore({ user: { loggedIn: false } })
+    const { getByText, getByTestId } = render(
+      <Root store={store}>
+        <Theme>
+          <Router>
+            <Navigation />
+          </Router>
+        </Theme>
+      </Root>,
     )
-    expect(getByText('Home').tagName).toBe('A')
-    expect(getByText('New').tagName).toBe('A')
-    expect(getByText('Login').tagName).toBe('A')
-    // TODO: find a way to check for the settings icon
+    expect(getByText('Library')).toBeDefined()
+    expect(getByText('New')).toBeDefined()
+    expect(getByText('Login')).toBeDefined()
+    expect(getByTestId('icon')).toBeDefined()
+  })
+
+  it('renders differently when logged in', () => {
+    store = mockStore({ user: { loggedIn: true } })
+    const { getByText, getByTestId } = render(
+      <Root store={store}>
+        <Theme>
+          <Router>
+            <Navigation />
+          </Router>
+        </Theme>
+      </Root>,
+    )
+    expect(getByText('Library')).toBeDefined()
+    expect(getByText('New')).toBeDefined()
+    expect(getByText('Account')).toBeDefined()
+    expect(getByTestId('icon')).toBeDefined()
   })
 })
