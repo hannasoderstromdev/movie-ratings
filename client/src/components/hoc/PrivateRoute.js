0f99ca33bc5cdca529da3e623992651f8a500b85
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-31 11:31:45 +0100

    feat: handle 403 in PrivateRoute hoc

--- client/src/components/hoc/PrivateRoute.js
+++ client/src/components/hoc/PrivateRoute.js
@@ -1,24 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      localStorage.getItem('user') ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{ pathname: '/login', state: { from: props.location } }}
-        />
-      )
-    }
-  />
-)
+const PrivateRoute = ({ component: Component, errorHandler, ...rest }) => {
+  // Only checking for existing localStorage item doesn't cover when item exist but token is invalid. Hence, checking for 403 errors is needed.
+  if (errorHandler.status === 403) {
+    return (
+      <Route
+        {...rest}
+        render={props => (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }}
+          />
+        )}
+      />
+    )
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem('user') ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  )
+}
 
 PrivateRoute.propTypes = {
   component: PropTypes.any.isRequired,
+  errorHandler: PropTypes.shape({
+    error: PropTypes.bool.isRequired,
+    status: PropTypes.number.isRequired,
+    message: PropTypes.string,
+    type: PropTypes.string,
+  }).isRequired,
 }
 
-export default PrivateRoute
+const mapStateToProps = ({ errorHandler }) => ({
+  errorHandler,
+})
+
+export default connect(mapStateToProps)(PrivateRoute)
