a05663b2558f5e2b2b6893f7a88850c736d611db
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-31 16:55:10 +0100

    feat: handle update movie

--- client/src/reducers/movies/movies.reducer.js
+++ client/src/reducers/movies/movies.reducer.js
@@ -2,6 +2,9 @@ import {
   GET_ALL_MOVIES,
   GET_ALL_MOVIES_SUCCESS,
   GET_ALL_MOVIES_FAILURE,
+  UPDATE_MOVIE,
+  UPDATE_MOVIE_FAILURE,
+  UPDATE_MOVIE_SUCCESS,
 } from 'actions/movies/movies.actions'
 
 const initialState = {
@@ -14,20 +17,50 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case GET_ALL_MOVIES:
       return {
-        ...initialState,
+        ...state,
         loading: true,
       }
 
     case GET_ALL_MOVIES_SUCCESS:
       return {
-        ...initialState,
+        ...state,
+        error: false,
+        loading: false,
         movies: action.payload.movies,
       }
 
     case GET_ALL_MOVIES_FAILURE:
       return {
-        ...initialState,
+        ...state,
         error: true,
+        loading: false,
+      }
+
+    case UPDATE_MOVIE:
+      return {
+        ...state,
+        error: false,
+        loading: true,
+      }
+
+    case UPDATE_MOVIE_FAILURE:
+      return {
+        ...state,
+        error: true,
+        loading: false,
+      }
+
+    case UPDATE_MOVIE_SUCCESS:
+      return {
+        ...state,
+        error: false,
+        loading: false,
+        movies: state.movies.map(movie => {
+          if (movie.id === action.payload.movie.id) {
+            return action.payload.movie
+          }
+          return movie
+        }),
       }
 
     default:
