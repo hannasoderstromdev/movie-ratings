3ae0fe455209e8c749a93e6689b7af028ba95c55
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-31 11:31:31 +0100

    test: add for onChange

--- client/src/components/atoms/InputField/__tests__/InputField.spec.js
+++ client/src/components/atoms/InputField/__tests__/InputField.spec.js
@@ -1,23 +1,42 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 
 import InputField from '..'
 import Theme from '../../../Theme'
 
 describe('<InputField />', () => {
-  it('renders', () => {
-    const props = {
+  let onChange
+  let props
+
+  beforeEach(() => {
+    onChange = jest.fn()
+    props = {
       name: 'name',
       type: 'text',
       placeholder: 'placeholder',
       value: '',
-      onChange: jest.fn(),
+      onChange,
     }
+  })
+
+  afterEach(cleanup)
+
+  it('renders', () => {
     const { getByTestId } = render(
       <Theme>
         <InputField {...props} />
       </Theme>,
     )
     expect(getByTestId('input').tagName).toBe('INPUT')
+  })
+
+  it('handles input', () => {
+    const { getByTestId } = render(
+      <Theme>
+        <InputField {...props} />
+      </Theme>,
+    )
+    fireEvent.change(getByTestId('input'), { target: { value: 'A' } })
+    expect(onChange).toHaveBeenCalledTimes(1)
   })
 })
