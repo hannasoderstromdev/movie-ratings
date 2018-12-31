29366165861693ab7f50d12bb0161d9948702d45
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-31 11:35:16 +0100

    feat: new component

---
+++ client/src/components/pages/Error404/Error404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Page from 'components/templates/Page'
+import Main from 'components/templates/Main'
+import { H1 } from 'components/atoms/Typography'
+
+const Error404 = () => (
+  <Page>
+    <Main>
+      <H1>No such page</H1>
+    </Main>
+  </Page>
+)
+
+export default Error404
