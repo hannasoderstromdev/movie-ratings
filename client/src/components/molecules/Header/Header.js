773d99d66c8bd4bb6c42859294ea5efed613a267
Hanna Söderström <gothbarbie84@gmail.com>
2018-12-31 12:53:17 +0100

    feat: add logo

--- client/src/components/molecules/Header/Header.js
+++ client/src/components/molecules/Header/Header.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import logoImg from './logo.svg'
+import clapperImg from './clapper.svg'
+
+const Logo = styled.img`
+  max-height: 2.25vh;
+`
+
+const Clapper = styled.img`
+  max-height: 2.25vh;
+  margin-right: 1rem;
+`
+
 const HeaderStyle = styled.header`
   width: 100%;
   height: 8vh;
@@ -10,11 +22,17 @@ const HeaderStyle = styled.header`
   background-color: ${({ theme }) => theme.colors.dark};
   color: ${({ theme }) => theme.colors.primary};
   font-family: 'Trajan Pro 3', serif;
+  line-height: 1rem;
   font-size: 1.8rem;
   padding: 0;
   margin: 0;
 `
 
-const Header = () => <HeaderStyle>Movie Ratings</HeaderStyle>
+const Header = () => (
+  <HeaderStyle>
+    <Clapper src={clapperImg} alt="logo" />
+    <Logo src={logoImg} alt="movie ratings" />
+  </HeaderStyle>
+)
 
 export default Header
