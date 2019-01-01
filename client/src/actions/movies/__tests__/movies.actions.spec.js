ab13bb9c8aea5a783e5dcd49c1e2a4798d63f40c
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-01 12:36:28 +0100

    test: refactor thunk tests

--- client/src/actions/movies/__tests__/movies.actions.spec.js
+++ client/src/actions/movies/__tests__/movies.actions.spec.js
@@ -1,6 +1,3 @@
-import configureStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-
 import {
   GET_ALL_MOVIES,
   GET_ALL_MOVIES_SUCCESS,
@@ -14,12 +11,7 @@ import {
   createMovieAction,
   createMovieSuccessAction,
   createMovieFailureAction,
-  getAllMovies,
-  createMovie,
 } from '../movies.actions'
-
-const middlewares = [thunk]
-const mockStore = configureStore(middlewares)
 
 describe('Actions/Movies', () => {
   describe('getAllMoviesAction', () => {
@@ -35,7 +27,7 @@ describe('Actions/Movies', () => {
   describe('getAllMoviesSuccessAction', () => {
     it(`returns ${GET_ALL_MOVIES_SUCCESS} and correct payload`, () => {
       const movies = { data: [] }
-      const action = getAllMoviesSuccessAction({ movies: movies.data })
+      const action = getAllMoviesSuccessAction({ movies: movies })
 
       expect(action).toEqual({
         type: GET_ALL_MOVIES_SUCCESS,
@@ -82,58 +74,6 @@ describe('Actions/Movies', () => {
 
       expect(action).toEqual({
         type: CREATE_MOVIE_FAILURE,
-      })
-    })
-  })
-
-  describe('Thunks', () => {
-    let initialState
-    let store
-
-    beforeEach(() => {
-      initialState = {}
-      store = mockStore(initialState)
-    })
-
-    // TODO: mock fetch
-    xdescribe('getAllMovies', () => {
-      it('dispatches the correct actions', async () => {
-        await store.dispatch(getAllMovies())
-        const actions = store.getActions()
-        const expected = [
-          { type: 'GET_ALL_MOVIES' },
-          {
-            payload: { error: 'Network request failed' },
-            type: 'GET_ALL_MOVIES_FAILURE',
-          },
-          {
-            payload: { message: 'Network request failed' },
-            type: 'ALERT_ERROR',
-          },
-        ]
-
-        expect(actions).toEqual(expected)
-      })
-    })
-
-    // TODO: mock fetch
-    xdescribe('createMovie', () => {
-      it('dispatches the correct actions', async () => {
-        await store.dispatch(createMovie())
-        const actions = store.getActions()
-        const expected = [
-          { type: 'CREATE_MOVIE' },
-          {
-            payload: { error: 'Network request failed' },
-            type: 'CREATE_MOVIE_FAILURE',
-          },
-          {
-            payload: { message: 'Network request failed' },
-            type: 'ALERT_ERROR',
-          },
-        ]
-
-        expect(actions).toEqual(expected)
       })
     })
   })
