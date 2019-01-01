53b4f1b0412c64377d53471dc7669181a61d4d6b
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-01 12:36:35 +0100

    test: refactor thunks

---
+++ client/src/actions/movies/__tests__/movies.thunks.spec.js
@@ -0,0 +1,59 @@
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import { getAllMovies, createMovie } from '../movies.thunks'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+describe('Actions/Movies/Thunks', () => {
+  let initialState
+  let store
+
+  beforeEach(() => {
+    initialState = {}
+    store = mockStore(initialState)
+  })
+
+  // TODO: mock fetch
+  xdescribe('getAllMovies', () => {
+    it('dispatches the correct actions', async () => {
+      await store.dispatch(getAllMovies())
+      const actions = store.getActions()
+      const expected = [
+        { type: 'GET_ALL_MOVIES' },
+        {
+          payload: { error: 'Network request failed' },
+          type: 'GET_ALL_MOVIES_FAILURE',
+        },
+        {
+          payload: { message: 'Network request failed' },
+          type: 'ALERT_ERROR',
+        },
+      ]
+
+      expect(actions).toEqual(expected)
+    })
+  })
+
+  // TODO: mock fetch
+  xdescribe('createMovie', () => {
+    it('dispatches the correct actions', async () => {
+      await store.dispatch(createMovie())
+      const actions = store.getActions()
+      const expected = [
+        { type: 'CREATE_MOVIE' },
+        {
+          payload: { error: 'Network request failed' },
+          type: 'CREATE_MOVIE_FAILURE',
+        },
+        {
+          payload: { message: 'Network request failed' },
+          type: 'ALERT_ERROR',
+        },
+      ]
+
+      expect(actions).toEqual(expected)
+    })
+  })
+})
