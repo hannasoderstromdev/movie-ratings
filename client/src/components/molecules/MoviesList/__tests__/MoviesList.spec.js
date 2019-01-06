ee40302d75e3b18517166e393bfd052de851002a
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-06 14:48:07 +0100

    test: fix broken test

--- client/src/components/molecules/MoviesList/__tests__/MoviesList.spec.js
+++ client/src/components/molecules/MoviesList/__tests__/MoviesList.spec.js
@@ -3,6 +3,8 @@ import { render } from 'react-testing-library'
 
 import MoviesList from '..'
 import Theme from '../../../Theme'
+import Root from 'components/Root'
+import { store } from 'helpers/store'
 
 describe('Components/Molecules/MoviesList', () => {
   it('renders', () => {
@@ -57,9 +59,11 @@ describe('Components/Molecules/MoviesList', () => {
       ],
     }
     const { container } = render(
-      <Theme>
-        <MoviesList {...props} />
-      </Theme>,
+      <Root store={store}>
+        <Theme>
+          <MoviesList {...props} />
+        </Theme>
+      </Root>,
     )
     expect(container.firstChild.tagName).toBe('DIV')
     expect(container.firstChild.children.length).toBe(2)
