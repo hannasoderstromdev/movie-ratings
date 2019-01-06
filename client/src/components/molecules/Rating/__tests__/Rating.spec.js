ddb7067fee126961667e99bf438245af39ad9810
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-06 14:48:35 +0100

    test: update for new useLock prop

--- client/src/components/molecules/Rating/__tests__/Rating.spec.js
+++ client/src/components/molecules/Rating/__tests__/Rating.spec.js
@@ -1,25 +1,55 @@
 import React from 'react'
-import { render, fireEvent } from 'react-testing-library'
+import { render, fireEvent, cleanup } from 'react-testing-library'
 
 import Rating from '..'
 import Theme from '../../../Theme'
 
 describe('Components/Molecules/Rating', () => {
   const setRating = jest.fn()
-  const props = {
-    setRating,
-    rating: 1,
-  }
-  const { getByTestId } = render(
-    <Theme>
-      <Rating {...props} />
-    </Theme>,
-  )
+  let props
+
+  afterEach(cleanup)
+
   it('renders', () => {
+    props = {
+      setRating,
+      rating: 1,
+      useLock: false,
+    }
+    const { getByTestId } = render(
+      <Theme>
+        <Rating {...props} />
+      </Theme>,
+    )
     expect(getByTestId('rating').children.length).toBe(5)
   })
 
+  it('handles useLock', () => {
+    props = {
+      setRating,
+      rating: 1,
+      useLock: true,
+    }
+    const { getByTestId } = render(
+      <Theme>
+        <Rating {...props} />
+      </Theme>,
+    )
+    fireEvent.click(getByTestId('rating').children[1])
+    expect(setRating).not.toHaveBeenCalled()
+  })
+
   it('handles setRating', () => {
+    props = {
+      setRating,
+      rating: 1,
+      useLock: false,
+    }
+    const { getByTestId } = render(
+      <Theme>
+        <Rating {...props} />
+      </Theme>,
+    )
     fireEvent.click(getByTestId('rating').children[1])
     expect(setRating).toHaveBeenCalledTimes(1)
     expect(setRating).toHaveBeenCalledWith(2)
