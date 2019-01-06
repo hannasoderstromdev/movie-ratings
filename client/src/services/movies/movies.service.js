981d09dc8c6dd2db444683f77486952a01bf40b0
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-06 15:33:12 +0100

    refactor: with use of headers helper

--- client/src/services/movies/movies.service.js
+++ client/src/services/movies/movies.service.js
@@ -1,78 +1,34 @@
-// import authHeader from '../auth-header'
-
 import handleResponse from '../handle-response'
+import headers from 'helpers/headers'
 
 const getAll = async () => {
-  const user = await JSON.parse(localStorage.getItem('user'))
+  const options = await headers('GET::AUTH')
+  const response = await fetch('/movies', options)
+  return await handleResponse(response)
+}
 
-  if (user) {
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'x-access-token': user.token,
-      },
-    }
-
-    const response = await fetch('/movies', requestOptions)
-    return await handleResponse(response)
-  }
+const getLatest = async (amount = 10) => {
+  const options = await headers('GET::AUTH')
+  const response = await fetch(`/movies/latest/${amount}`, options)
+  return await handleResponse(response)
 }
 
 const create = async movie => {
-  const user = JSON.parse(localStorage.getItem('user'))
-
-  if (user) {
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'x-access-token': user.token,
-      },
-      body: JSON.stringify({ ...movie }),
-    }
-
-    const response = await fetch('/movies/create', requestOptions)
-    return await handleResponse(response)
-  }
+  const options = await headers('POST::AUTH', movie)
+  const response = await fetch('/movies/create', options)
+  return await handleResponse(response)
 }
 
 const update = async (movieId, newProps) => {
-  const user = JSON.parse(localStorage.getItem('user'))
-
-  if (user) {
-    const requestOptions = {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'x-access-token': user.token,
-      },
-      body: JSON.stringify({ ...newProps }),
-    }
-
-    const response = await fetch(`/movies/${movieId}`, requestOptions)
-    return await handleResponse(response)
-  }
+  const options = await headers('PUT::AUTH', newProps)
+  const response = await fetch(`/movies/${movieId}`, options)
+  return await handleResponse(response)
 }
 
 const deleteById = async id => {
-  const user = JSON.parse(localStorage.getItem('user'))
-
-  if (user) {
-    const requestOptions = {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'x-access-token': user.token,
-      },
-    }
-
-    const response = await fetch(`/movies/${id}`, requestOptions)
-    return await handleResponse(response)
-  }
+  const options = await headers('DELETE::AUTH')
+  const response = await fetch(`/movies/${id}`, options)
+  return await handleResponse(response)
 }
 
 const moviesService = { getAll, create, update, deleteById }
