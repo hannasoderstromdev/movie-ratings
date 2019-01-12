fd098c241609e60a2b7758d601189762ce5a0e49
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-12 12:58:50 +0100

    test: fix broken test

--- client/src/components/molecules/Star/__tests__/Star.spec.js
+++ client/src/components/molecules/Star/__tests__/Star.spec.js
@@ -3,7 +3,7 @@ import { render, fireEvent } from 'react-testing-library'
 
 import Star from '..'
 
-describe('Components/Star', () => {
+describe('Components/Molecules/Star', () => {
   const onMouseOver = jest.fn()
   const onMouseOut = jest.fn()
   const onClick = jest.fn()
@@ -13,22 +13,26 @@ describe('Components/Star', () => {
     onClick,
     isSelected: true,
   }
-  const { getByTestId } = render(<Star {...props} />)
+
   it('renders', () => {
+    const { getByTestId, debug } = render(<Star {...props} />)
     expect(getByTestId('star').tagName).toBe('BUTTON')
   })
 
   it('handles click', () => {
+    const { getByTestId } = render(<Star {...props} />)
     fireEvent.click(getByTestId('star'))
     expect(onClick).toHaveBeenCalledTimes(1)
   })
 
   it('handles mouseOver', () => {
+    const { getByTestId } = render(<Star {...props} />)
     fireEvent.mouseOver(getByTestId('star').children[0])
     expect(onMouseOver).toHaveBeenCalledTimes(1)
   })
 
   it('handles mouseOut', () => {
+    const { getByTestId } = render(<Star {...props} />)
     fireEvent.mouseOut(getByTestId('star').children[0])
     expect(onMouseOut).toHaveBeenCalledTimes(1)
   })
