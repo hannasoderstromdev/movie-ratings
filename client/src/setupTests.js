67d49ec03ed78d5219025d5455a781bcdcb49240
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-12 12:19:10 +0100

    feat: add extensions to expect and cleanup

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -14,6 +14,13 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import 'jest-localstorage-mock'
 
+// Adds additional expect-functions
+import 'jest-dom/extend-expect'
+import 'jest-axe/extend-expect'
+
+// Cleans up DOM after each test automatically
+import 'react-testing-library/cleanup-after-each'
+
 library.add(
   faCheckCircle,
   faChevronDown,
