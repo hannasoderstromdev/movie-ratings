bff66281c503729abbeffb9ff006218a5298d5e1
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-12 14:34:02 +0100

    test: fix excempted tests

--- client/src/actions/movies/__tests__/movies.thunks.spec.js
+++ client/src/actions/movies/__tests__/movies.thunks.spec.js
@@ -1,5 +1,6 @@
 import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
 
 import moviesThunks from '../movies.thunks'
 
@@ -9,26 +10,37 @@ const mockStore = configureStore(middlewares)
 describe('Actions/Movies/Thunks', () => {
   let initialState
   let store
+  fetchMock.config.overwriteRoutes = true
 
   beforeEach(() => {
     initialState = {}
     store = mockStore(initialState)
+    localStorage.setItem('user', JSON.stringify({ token: 'fake token' }))
+  })
+
+  afterEach(() => {
+    localStorage.clear()
   })
 
   // TODO: mock fetch
-  xdescribe('getAllMovies', () => {
+  describe('getAllMovies', () => {
     it('dispatches the correct actions', async () => {
+      fetchMock.mock('/movies', {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { data: [{}] },
+      })
       await store.dispatch(moviesThunks.getAllMovies())
       const actions = store.getActions()
       const expected = [
         { type: 'GET_ALL_MOVIES' },
         {
-          payload: { error: 'Network request failed' },
-          type: 'GET_ALL_MOVIES_FAILURE',
-        },
-        {
-          payload: { message: 'Network request failed' },
-          type: 'ALERT_ERROR',
+          payload: {
+            movies: [{}],
+          },
+          type: 'GET_ALL_MOVIES_SUCCESS',
         },
       ]
 
@@ -37,19 +49,24 @@ describe('Actions/Movies/Thunks', () => {
   })
 
   // TODO: mock fetch
-  xdescribe('createMovie', () => {
+  describe('createMovie', () => {
     it('dispatches the correct actions', async () => {
+      fetchMock.mock('/movies/create', {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { message: 'Movie added successfully' },
+      })
       await store.dispatch(moviesThunks.createMovie())
       const actions = store.getActions()
       const expected = [
         { type: 'CREATE_MOVIE' },
         {
-          payload: { error: 'Network request failed' },
-          type: 'CREATE_MOVIE_FAILURE',
-        },
-        {
-          payload: { message: 'Network request failed' },
-          type: 'ALERT_ERROR',
+          payload: {
+            movie: { message: 'Movie added successfully' },
+          },
+          type: 'CREATE_MOVIE_SUCCESS',
         },
       ]
 
