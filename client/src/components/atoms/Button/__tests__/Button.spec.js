b36be774d975f16b82d75d7bf9990a8ffbb62c3a
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-12 12:19:22 +0100

    test: add scenarios

--- client/src/components/atoms/Button/__tests__/Button.spec.js
+++ client/src/components/atoms/Button/__tests__/Button.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render, cleanup, fireEvent } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
+import { axe } from 'jest-axe'
 
 import Button from '..'
 import Theme from '../../../Theme'
@@ -16,24 +17,89 @@ describe('<Button />', () => {
     }
   })
 
-  afterEach(cleanup)
-
   it('renders a button', () => {
     const { getByTestId } = render(
       <Theme>
         <Button {...props}>Test Button</Button>
       </Theme>,
     )
-    expect(getByTestId('primary-button').tagName).toBe('BUTTON')
+    const button = getByTestId('primary-button')
+    expect(button).toBeDefined()
+    expect(button).toHaveTextContent('Test Button')
+  })
+
+  describe('handles props', () => {
+    it('type submit', () => {
+      props = {
+        type: 'submit',
+      }
+      const { getByTestId } = render(
+        <Theme>
+          <Button {...props}>Test Button</Button>
+        </Theme>,
+      )
+      const button = getByTestId('primary-button')
+      expect(button).toHaveAttribute('type', 'submit')
+    })
+
+    it('disabled', () => {
+      props = {
+        disabled: true,
+      }
+      const { debug, getByTestId } = render(
+        <Theme>
+          <Button {...props}>Test Button</Button>
+        </Theme>,
+      )
+      const button = getByTestId('primary-button')
+      expect(button).toHaveAttribute('disabled', '')
+    })
+
+    it('secondary', () => {
+      props = {
+        secondary: true,
+      }
+      const { debug, getByTestId } = render(
+        <Theme>
+          <Button {...props}>Test Button</Button>
+        </Theme>,
+      )
+      const button = getByTestId('secondary-button')
+      expect(button).toBeDefined()
+    })
+
+    it('thirdiary', () => {
+      props = {
+        thirdiary: true,
+      }
+      const { debug, getByTestId } = render(
+        <Theme>
+          <Button {...props}>Test Button</Button>
+        </Theme>,
+      )
+      const button = getByTestId('thirdiary-button')
+      expect(button).toBeDefined()
+    })
   })
 
   it('runs onClick when clicked', () => {
-    const { getByTestId } = render(
+    const { getByTestId, debug } = render(
       <Theme>
         <Button {...props}>Test Button</Button>
       </Theme>,
     )
-    fireEvent.click(getByTestId('primary-button'))
+    const button = getByTestId('primary-button')
+    fireEvent.click(button)
     expect(onClick).toHaveBeenCalledTimes(1)
+  })
+
+  it('is accessible', async () => {
+    const { container } = render(
+      <Theme>
+        <Button {...props}>Test Button</Button>
+      </Theme>,
+    )
+    const results = await axe(container.innerHTML)
+    expect(results).toHaveNoViolations()
   })
 })
