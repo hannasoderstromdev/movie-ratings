9a59b8513c501ee3fa04d3c7a0dd2c28ac9f3c19
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-12 12:20:40 +0100

    test: add assertion for accessibility

--- client/src/components/molecules/LabeledInputField/__tests__/LabeledInputField.spec.js
+++ client/src/components/molecules/LabeledInputField/__tests__/LabeledInputField.spec.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { render } from 'react-testing-library'
+import { axe } from 'jest-axe'
 
 import LabeledInputField from '..'
 import Theme from '../../../Theme'
 
 describe('Components/Molecules/LabeledInputField', () => {
+  const props = {
+    label: 'Label',
+    name: 'thing',
+    type: 'text',
+    onChange: jest.fn(),
+    value: '',
+    placeholder: 'enter text',
+  }
+
   it('renders', () => {
-    const props = {
-      label: 'Label',
-      name: 'thing',
-      type: 'text',
-      onChange: jest.fn(),
-      value: '',
-      placeholder: 'enter text',
-    }
     const { getByText, getByTestId } = render(
       <Theme>
         <LabeledInputField {...props} />
@@ -21,5 +23,15 @@ describe('Components/Molecules/LabeledInputField', () => {
     )
     expect(getByText('Label').tagName).toBe('LABEL')
     expect(getByTestId('input').tagName).toBe('INPUT')
+  })
+
+  it('is accessible', async () => {
+    const { container } = render(
+      <Theme>
+        <LabeledInputField {...props} />
+      </Theme>,
+    )
+    const results = await axe(container.innerHTML)
+    expect(results).toHaveNoViolations()
   })
 })
