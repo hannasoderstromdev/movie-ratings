915b61418e0716e22b2047b1aa7652e7d2b80293
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-17 17:15:05 +0100

    feat: add amount prop to latest

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -4,7 +4,7 @@ const router = new Router()
 const movieController = require('../controllers/movie.controller')
 
 router.get('/', movieController.getAll)
-router.get('/latest', movieController.getLatest)
+router.get('/latest/:amount', movieController.getLatest)
 router.post('/create', movieController.create)
 router.get('/:movieId', movieController.getById)
 router.put('/:movieId', movieController.updateById)
