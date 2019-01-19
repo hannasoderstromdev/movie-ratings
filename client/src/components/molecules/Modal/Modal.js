a3f09c161b0236d166deed4fe55df7ddb8e529a7
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-19 11:19:41 +0100

    feat: new component

---
+++ client/src/components/molecules/Modal/Modal.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import Button from 'components/atoms/Button'
+
+const ModalWrapper = styled.div`
+  display: flex;
+  position: fixed;
+  background-color: ${({ theme }) => theme.color.darkTransparent};
+  z-index: ${({ zIndex }) => zIndex};
+`
+const ModalBox = styled.section`
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+`
+
+class Modal extends Component {
+  onClose = () => {
+    const { item, onClose } = this.props
+    if (item.onClose) {
+      item.onClose()
+      onClose(item.id)
+    } else {
+      onClose(item.id)
+    }
+  }
+
+  onConfirm = () => {
+    const { item, onClose } = this.props
+    if (item.onConfirm) {
+      item.onConfirm()
+      onClose(item).id
+    }
+  }
+
+  render() {
+    const { zIndex, item } = this.props
+
+    switch (item.type) {
+      case 'confirmation':
+        return (
+          <ModalWrapper zIndex={(zIndex + 1) * 10}>
+            <ModalBox>
+              <div>{item.text}</div>
+              <div>
+                <Button onClick={this.onConfirm}>Confirm</Button>
+                <Button onClick={this.onClose}>Close</Button>
+              </div>
+            </ModalBox>
+          </ModalWrapper>
+        )
+
+      case 'movie-details':
+        return (
+          <ModalWrapper zIndex={(zIndex + 1) * 10}>
+            <ModalBox>
+              <Button onClick={this.onClose}>Close</Button>
+              <div>{item.content}</div>
+            </ModalBox>
+          </ModalWrapper>
+        )
+
+      default:
+        return null
+    }
+  }
+}
+
+Modal.defaultProps = {
+  zIndex: 10,
+}
+
+Modal.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    onConfirm: PropTypes.func,
+    onClose: PropTypes.func,
+    text: PropTypes.string,
+    content: PropTypes.string,
+  }).isRequired,
+  onClose: PropTypes.func.isRequired,
+  zIndex: PropTypes.number,
+}
+
+export default Modal
