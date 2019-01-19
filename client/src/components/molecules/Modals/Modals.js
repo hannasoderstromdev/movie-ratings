f29e8b1db3015ac99abda234b680658436013f36
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-19 11:19:56 +0100

    feat: new component

---
+++ client/src/components/molecules/Modals/Modals.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import modalsActions from 'actions/modals/modals.actions'
+
+import Modal from 'components/molecules/Modal'
+
+const Modals = ({ modals, closeModal }) => (
+  <div>
+    {modals &&
+      modals.map((item, i) => (
+        <Modal
+          item={item}
+          key={item.id}
+          onClose={() => closeModal(item)}
+          zIndex={i}
+        />
+      ))}
+  </div>
+)
+
+Modals.propTypes = {
+  closeModal: PropTypes.func.isRequired,
+  modals: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string,
+      content: PropTypes.string,
+      onConfirm: PropTypes.func,
+    }),
+  ).isRequired,
+}
+
+const mapStateToProps = ({ modals }) => ({
+  modals,
+})
+
+const mapDispatchToProps = {
+  closeModal: modalsActions.closeModal,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Modals)
