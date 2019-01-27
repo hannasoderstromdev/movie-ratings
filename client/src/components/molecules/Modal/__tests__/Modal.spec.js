c1fa1ca22ba79aba7b4df03e37ec3772fa66645b
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-27 19:16:36 +0100

    test: add store for FullMovie

--- client/src/components/molecules/Modal/__tests__/Modal.spec.js
+++ client/src/components/molecules/Modal/__tests__/Modal.spec.js
@@ -1,56 +1,78 @@
 import React from 'react'
 import { render } from 'react-testing-library'
+import configureStore from 'redux-mock-store'
 
+import Root from 'components/Root'
 import Theme from 'components/Theme'
 
 import Modal from '..'
 
+const mockStore = configureStore([])
+
+let item
+let props
+let utils
+
 describe('Components/Molecules/Modal', () => {
-  it('renders confirmation modal', () => {
-    const item = {
+  beforeEach(() => {
+    const initialState = {
+      movies: {
+        movies: [
+          {
+            id: 'mid01',
+            actors: 'Tufsen',
+            country: 'Sweden',
+            genre: 'Drama',
+            director: 'Hanna Söderström',
+            language: 'SWE',
+            runtime: '120 min',
+            year: '2019',
+            plot:
+              'A cat that suffers from OCD struggles to leave things on the counter alone',
+            production: 'Cats and Kitties LTD',
+            title: 'CATch me if you can',
+            ratings: [],
+            released: 'May 1st 2019',
+            writer: 'Issy Dragonfly',
+            poster: 'http://www.poster.com/image.jpg',
+          },
+        ],
+      },
+    }
+    const store = mockStore(initialState)
+    item = {
       id: 'mid1',
       type: 'confirmation',
       content: {
         text: 'Do you confirm?',
       },
     }
-    const props = {
+    props = {
       item,
       onClose: jest.fn(),
     }
-    const { getByText } = render(
-      <Theme>
-        <Modal {...props} />
-      </Theme>,
+    utils = render(
+      <Root store={store}>
+        <Theme>
+          <Modal {...props} />
+        </Theme>
+      </Root>,
     )
-    const text = getByText('Do you confirm?')
+  })
+
+  it('renders confirmation modal', () => {
+    const text = utils.getByText('Do you confirm?')
     expect(text).toBeDefined()
-    const confirmButton = getByText('Confirm')
+    const confirmButton = utils.getByText('Confirm')
     expect(confirmButton).toBeDefined()
-    const closeButton = getByText('Close')
+    const closeButton = utils.getByText('Close')
     expect(closeButton).toBeDefined()
   })
 
   it('renders movie-details modal', () => {
-    const item = {
-      id: 'mid1',
-      type: 'movie-details',
-      content: {
-        text: 'Awesome movie',
-      },
-    }
-    const props = {
-      item,
-      onClose: jest.fn(),
-    }
-    const { getByText } = render(
-      <Theme>
-        <Modal {...props} />
-      </Theme>,
-    )
-    const text = getByText(item.content.text)
+    const text = utils.getByText(item.content.text)
     expect(text).toBeDefined()
-    const closeButton = getByText('Close')
+    const closeButton = utils.getByText('Close')
     expect(closeButton).toBeDefined()
   })
 })
