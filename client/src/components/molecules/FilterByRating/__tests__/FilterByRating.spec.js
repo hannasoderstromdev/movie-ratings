2c0e563583dd25a3bf7d5d634732c81b84bc3f72
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-27 19:01:52 +0100

    test: for FilterByRating

---
+++ client/src/components/molecules/FilterByRating/__tests__/FilterByRating.spec.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { render, fireEvent, wait } from 'react-testing-library'
+
+import Theme from 'components/Theme'
+
+import FilterByRating from '..'
+
+describe('components/molecules/FilterByRating', () => {
+  const filterByRating = jest.fn()
+  const props = {
+    filterByRating,
+  }
+  let utils
+
+  beforeEach(() => {
+    utils = render(
+      <Theme>
+        <FilterByRating {...props} />
+      </Theme>,
+    )
+  })
+
+  it('renders closed', () => {
+    expect(utils.getByTestId('selected')).toBeDefined()
+  })
+
+  it('opens onClick', () => {
+    fireEvent.click(utils.getByTestId('selected'))
+    expect(utils.getByTestId('dropdown')).toBeDefined()
+  })
+
+  it('closes onBlur', async () => {
+    fireEvent.click(utils.getByTestId('selected'))
+
+    fireEvent.blur(utils.getByTestId('selected'))
+
+    await wait(() => expect(utils.queryByTestId('dropdown')).toBeNull())
+  })
+
+  it('calls filterByRating onClick', () => {
+    fireEvent.click(utils.getByTestId('selected'))
+    expect(utils.getByTestId('filter-one')).toBeDefined()
+
+    fireEvent.click(utils.getByTestId('filter-one'))
+    expect(filterByRating).toHaveBeenCalledWith('1')
+  })
+})
