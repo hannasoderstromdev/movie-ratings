1f5678b1d688dbc198fee86fe7b9cac6a6713c9e
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-27 20:41:08 +0100

    test: fix test for modals

--- client/src/components/molecules/Modals/__tests__/Modals.spec.js
+++ client/src/components/molecules/Modals/__tests__/Modals.spec.js
@@ -8,30 +8,74 @@ import Theme from 'components/Theme'
 import Modals from '..'
 
 const mockStore = configureStore([])
-const initialState = {
-  modals: [
-    {
-      id: 'mid1',
-      type: 'confirmation',
-      content: {
-        text: 'Do you confirm?',
-      },
-    },
-  ],
-}
+
+let props
+let utils
 
 describe('Components/Molecules/Modals', () => {
-  it('renders', () => {
+  beforeEach(() => {
+    const initialState = {
+      modals: [
+        {
+          id: 'modalId01',
+          type: 'confirmation',
+          content: {
+            text: 'Do you confirm?',
+          },
+          onClose: jest.fn(),
+          onConfirm: jest.fn(),
+        },
+        {
+          id: 'modalId02',
+          type: 'movie-details',
+          content: {
+            movieId: 'mid01',
+          },
+          onClose: jest.fn(),
+          onConfirm: jest.fn(),
+        },
+      ],
+      movies: {
+        movies: [
+          {
+            id: 'mid01',
+            actors: 'Tufsen',
+            country: 'Sweden',
+            genre: 'Drama',
+            director: 'Hanna Söderström',
+            language: 'SWE',
+            runtime: '120 min',
+            year: '2019',
+            plot:
+              'A cat that suffers from OCD struggles to leave things on the counter alone',
+            production: 'Cats and Kitties LTD',
+            title: 'CATch me if you can',
+            ratings: [],
+            released: 'May 1st 2019',
+            writer: 'Issy Dragonfly',
+            poster: 'http://www.poster.com/image.jpg',
+          },
+        ],
+      },
+    }
     const store = mockStore(initialState)
-    const { getByTestId } = render(
+
+    props = {
+      closeModal: jest.fn(),
+    }
+
+    utils = render(
       <Root store={store}>
         <Theme>
-          <Modals />
+          <Modals {...props} />
         </Theme>
       </Root>,
     )
-    const modals = getByTestId('modals')
+  })
+
+  it('renders', () => {
+    const modals = utils.getByTestId('modals')
     expect(modals).toBeDefined()
-    expect(modals.children.length).toEqual(1)
+    expect(modals.children.length).toEqual(2)
   })
 })
