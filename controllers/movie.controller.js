0f0e43b048f3745d98b8d576ab2c59dcce6d762e
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-27 13:11:23 +0100

    feat: helper function for movie props

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,5 +1,31 @@
 const movieModel = require('../models/Movie')
 
+function filterValidMovieProps(props) {
+  return {
+    actors: props.actors,
+    awards: props.awards,
+    country: props.country,
+    director: props.director,
+    genre: props.genre,
+    imdbID: props.imdbID,
+    imdbRating: props.imdbRating,
+    imdbVotes: props.imdbVotes,
+    language: props.language,
+    metascore: props.metascore,
+    plot: props.plot,
+    poster: props.poster,
+    production: props.production,
+    rating: props.rating,
+    ratings: props.ratings,
+    released: props.released,
+    runtime: props.runtime,
+    title: props.title,
+    website: props.website,
+    writer: props.writer,
+    year: props.year,
+  }
+}
+
 function addIdToMovies(movies) {
   const moviesWithId = []
   for (const movie of movies) {
@@ -35,217 +61,33 @@ module.exports = {
   /**
    * Get Movie By ID
    */
-  getById: function(req, res, next) {
-    movieModel.findById(req.params.movieId, function(err, movieInfo) {
-      if (err) {
-        next(err)
-      } else {
-        res.json({
-          status: 'success',
-          message: 'Movie found',
-          data: { movies: movieInfo },
-        })
-      }
-    })
+  getById: async (req, res, next) => {
+    console.log('getById')
+    try {
+      const movieInfo = await movieModel.findById(req.params.movieId)
+      res.json({
+        status: 'success',
+        message: 'Movie found',
+        data: { movies: movieInfo },
+      })
+    } catch (error) {
+      next(error)
+    }
   },
 
   /**
    * Get all Movies
    */
-  getAll: function(req, res, next) {
-    const moviesList = []
+  getAll: async (req, res, next) => {
+    console.log('getAll')
 
-    movieModel.find({}, function(err, movies) {
-      if (err) {
-        next(err)
-      } else {
-        for (const {
-          _id,
-          actors,
-          awards,
-          country,
-          director,
-          genre,
-          imdbID,
-          imdbRating,
-          imdbVotes,
-          language,
-          metascore,
-          plot,
-          poster,
-          production,
-          rating,
-          ratings,
-          released,
-          runtime,
-          title,
-          website,
-          writer,
-          year,
-        } of movies) {
-          moviesList.push({
-            id: _id,
-            actors,
-            awards,
-            country,
-            director,
-            genre,
-            imdbID,
-            imdbRating,
-            imdbVotes,
-            language,
-            metascore,
-            plot,
-            poster,
-            production,
-            rating,
-            ratings,
-            released,
-            runtime,
-            title,
-            website,
-            writer,
-            year,
-          })
-        }
-        if (moviesList.length === 0) {
-          res.status(204).json({
-            status: 'success',
-            message: 'No movies available',
-            data: null,
-          })
-        } else {
-          res.status(200).json({
-            status: 'success',
-            message: 'All movies',
-            data: moviesList,
-          })
-        }
-      }
-    })
-  },
+    try {
+      const moviesList = []
 
-  /**
-   * Update Movie
-   */
-  updateById: function(req, res, next) {
-    const newValues = {}
-    if (req.body.title) newValues.title = req.body.title
-    if (req.body.year) newValues.year = req.body.year
-    if (req.body.runtime) newValues.runtime = req.body.runtime
-    if (req.body.director) newValues.director = req.body.director
-    if (req.body.actors) newValues.actors = req.body.actors
-    if (req.body.poster) newValues.poster = req.body.poster
-    if (req.body.imdbID) newValues.imdbID = req.body.imdbID
-    if (req.body.rating) newValues.rating = req.body.rating
+      const movies = await movieModel.find({})
 
-    movieModel.findByIdAndUpdate(
-      req.params.movieId,
-      { ...newValues },
-      { new: true, runValidators: true },
-      function(err, movieInfo) {
-        if (err) {
-          next(err)
-        } else {
-          const {
-            actors,
-            awards,
-            country,
-            director,
-            genre,
-            imdbID,
-            imdbRating,
-            imdbVotes,
-            language,
-            metascore,
-            plot,
-            poster,
-            production,
-            rating,
-            ratings,
-            released,
-            runtime,
-            title,
-            website,
-            writer,
-            year,
-          } = movieInfo
-          res.json({
-            status: 'success',
-            message: 'Movie updated successfully',
-            data: {
-              id: movieInfo._id,
-              actors,
-              awards,
-              country,
-              director,
-              genre,
-              imdbID,
-              imdbRating,
-              imdbVotes,
-              language,
-              metascore,
-              plot,
-              poster,
-              production,
-              rating,
-              ratings,
-              released,
-              runtime,
-              title,
-              website,
-              writer,
-              year,
-            },
-          })
-        }
-      },
-    )
-  },
-
-  /**
-   * Delete Movie
-   */
-  deleteById: function(req, res, next) {
-    movieModel.findByIdAndRemove(req.params.movieId, function(err, movieInfo) {
-      if (err) {
-        next(err)
-      } else {
-        res.json({
-          status: 'success',
-          message: 'Movie deleted successfully',
-          data: { id: movieInfo._id },
-        })
-      }
-    })
-  },
-
-  create: function(req, res, next) {
-    const {
-      actors,
-      awards,
-      country,
-      director,
-      genre,
-      imdbID,
-      imdbRating,
-      imdbVotes,
-      language,
-      metascore,
-      plot,
-      poster,
-      production,
-      rating,
-      ratings,
-      released,
-      runtime,
-      title,
-      website,
-      writer,
-      year,
-    } = req.body
-    movieModel.create(
-      {
+      for (const {
+        _id,
         actors,
         awards,
         country,
@@ -267,44 +109,140 @@ module.exports = {
         website,
         writer,
         year,
-      },
-      function(err, result) {
-        if (err) {
-          next(err)
-        } else {
-          res.json({
-            status: 'success',
-            message: 'Movie added successfully',
-            data: { id: result._id },
-          })
-        }
-      },
-    )
+      } of movies) {
+        moviesList.push({
+          id: _id,
+          actors,
+          awards,
+          country,
+          director,
+          genre,
+          imdbID,
+          imdbRating,
+          imdbVotes,
+          language,
+          metascore,
+          plot,
+          poster,
+          production,
+          rating,
+          ratings,
+          released,
+          runtime,
+          title,
+          website,
+          writer,
+          year,
+        })
+      }
+      if (moviesList.length === 0) {
+        res.status(204).json({
+          status: 'success',
+          message: 'No movies available',
+          data: null,
+        })
+      } else {
+        res.status(200).json({
+          status: 'success',
+          message: 'All movies',
+          data: moviesList,
+        })
+      }
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * Update Movie
+   */
+  updateById: async (req, res, next) => {
+    const newValues = {}
+
+    if (req.body.title) newValues.title = req.body.title
+    if (req.body.year) newValues.year = req.body.year
+    if (req.body.runtime) newValues.runtime = req.body.runtime
+    if (req.body.director) newValues.director = req.body.director
+    if (req.body.actors) newValues.actors = req.body.actors
+    if (req.body.poster) newValues.poster = req.body.poster
+    if (req.body.imdbID) newValues.imdbID = req.body.imdbID
+    if (req.body.rating) newValues.rating = req.body.rating
+
+    try {
+      const movieInfo = await movieModel.findByIdAndUpdate(
+        req.params.movieId,
+        { ...newValues },
+        { new: true, runValidators: true },
+      )
+
+      res.json({
+        status: 'success',
+        message: 'Movie updated successfully',
+        data: {
+          id: movieInfo._id,
+          ...filterValidMovieProps(movieInfo),
+        },
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * Delete Movie
+   */
+  deleteById: async (req, res, next) => {
+    try {
+      const movieInfo = await movieModel.findByIdAndRemove(req.params.movieId)
+
+      res.json({
+        status: 'success',
+        message: 'Movie deleted successfully',
+        data: { id: movieInfo._id },
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  create: async (req, res, next) => {
+    try {
+      const result = await movieModel.create({
+        ...filterValidMovieProps(req.body),
+      })
+      console.log('result', result)
+
+      res.json({
+        status: 'success',
+        message: 'Movie added successfully',
+        data: { id: result._id },
+      })
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
   },
 
   // Middleware
   // depends on search.controller.search to set res.locals
-  findExisting: function(req, res, next) {
+  findExisting: async (req, res, next) => {
     // compare result to database titles and find out if it already exists
-    movieModel.find(
-      {
+    try {
+      const result = await movieModel.find({
         title: res.locals.data.title,
-      },
-      function(err, result) {
-        if (err) {
-          next(err)
-        } else {
-          if (result.length) {
-            res.json({ ...res.locals, inLibrary: true })
-          } else {
-            res.json({ ...res.locals, inLibrary: false })
-          }
-        }
-      },
-    )
+      })
+
+      if (result.length) {
+        res.json({ ...res.locals, inLibrary: true })
+      } else {
+        res.json({ ...res.locals, inLibrary: false })
+      }
+    } catch (error) {
+      next(error)
+    }
   },
 
-  getLatest: async function(req, res, next) {
+  getLatest: async (req, res, next) => {
     // get the latest X nr of movies added
     try {
       const result = await movieModel
@@ -325,16 +263,16 @@ module.exports = {
     }
   },
 
-  findByTitle: async function(req, res, next) {
+  findByTitle: async (req, res, next) => {
+    console.log('findByTitle')
     try {
       const result = await movieModel
         .find({
           $text: {
-            $search: req.params.title.toLowerCase(),
+            $search: `\"${req.params.title.toLowerCase()}\"`,
           },
         })
         .exec()
-      console.log('findByTitle', result)
 
       const movieWithId = addIdToMovies(result)
 
@@ -344,6 +282,7 @@ module.exports = {
         data: movieWithId,
       })
     } catch (error) {
+      console.error(error)
       next(error)
     }
   },
