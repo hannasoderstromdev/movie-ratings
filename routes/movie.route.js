6465e2a3881caf56c74323c0372dc0e086e727cb
Hanna Söderström <gothbarbie84@gmail.com>
2019-01-27 14:43:13 +0100

    feat: implement validation middleware

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -4,11 +4,17 @@ const router = new Router()
 const movieController = require('../controllers/movie.controller')
 
 router.get('/', movieController.getAll)
-router.get('/title/:title', movieController.findByTitle)
+
+router.get('/title/:title', movieController.validate('findByTitle'), movieController.findByTitle)
+
 router.get('/latest/:amount', movieController.getLatest)
-router.post('/create', movieController.create)
-router.get('/:movieId(d+)', movieController.getById)
-router.put('/:movieId', movieController.updateById)
-router.delete('/:movieId', movieController.deleteById)
+
+router.post('/create', movieController.validate('create'), movieController.create)
+
+router.get('/:movieId(d+)', movieController.validate('getById'), movieController.getById)
+
+router.put('/:movieId', movieController.validate('updateById'), movieController.updateById)
+
+router.delete('/:movieId', movieController.validate('deleteById'), movieController.deleteById)
 
 module.exports = router
