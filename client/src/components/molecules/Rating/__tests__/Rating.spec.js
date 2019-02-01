2ac615ef0e28095c29aa6560baed55a34e7ac6e1
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-01 22:33:27 +0100

    test: update

--- client/src/components/molecules/Rating/__tests__/Rating.spec.js
+++ client/src/components/molecules/Rating/__tests__/Rating.spec.js
@@ -1,56 +1,38 @@
 import React from 'react'
-import { render, fireEvent, cleanup } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 
 import Rating from '..'
 import Theme from '../../../Theme'
 
 describe('Components/Molecules/Rating', () => {
   const setRating = jest.fn()
+  const onMouseOver = jest.fn()
+  const onMouseOut = jest.fn()
   let props
+  let utils
 
-  afterEach(cleanup)
-
-  it('renders', () => {
+  beforeEach(() => {
     props = {
       setRating,
-      rating: 1,
-      useLock: false,
-    }
-    const { getByTestId } = render(
-      <Theme>
-        <Rating {...props} />
-      </Theme>,
-    )
-    expect(getByTestId('rating').children.length).toBe(5)
-  })
-
-  it('handles useLock', () => {
-    props = {
-      setRating,
+      onMouseOver,
+      onMouseOut,
       rating: 1,
       useLock: true,
     }
-    const { getByTestId } = render(
+    utils = render(
       <Theme>
         <Rating {...props} />
       </Theme>,
     )
-    fireEvent.click(getByTestId('rating').children[1])
-    expect(setRating).not.toHaveBeenCalled()
   })
 
-  it('handles setRating', () => {
-    props = {
-      setRating,
-      rating: 1,
-      useLock: false,
-    }
-    const { getByTestId } = render(
-      <Theme>
-        <Rating {...props} />
-      </Theme>,
-    )
-    fireEvent.click(getByTestId('rating').children[1])
+  it('renders', () => {
+    expect(utils.getByTestId('rating').children.length).toBe(6) // 5 without lock
+  })
+
+  it('handles useLock and setRating', () => {
+    fireEvent.click(utils.getByTestId('rating').children[5]) // unlock
+    fireEvent.click(utils.getByTestId('rating').children[1])
     expect(setRating).toHaveBeenCalledTimes(1)
     expect(setRating).toHaveBeenCalledWith(2)
   })
