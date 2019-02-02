119964f844db1cdad0fe09e7df257f14d18f3c8a
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-02 10:49:32 +0100

    test: for RateNewMovie

---
+++ client/src/components/organisms/RateNewMovie/__tests__/RateNewMovie.spec.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import { BrowserRouter as Router } from 'react-router-dom'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import Root from 'components/Root'
+import Theme from 'components/Theme'
+
+import RateNewMovie from '..'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+let utils
+let props
+let store
+let state
+
+describe('Components/Organisms/RateNewMovie', () => {
+  beforeEach(() => {
+    state = {
+      search: {
+        loading: false,
+        movie: {
+          id: 'mId01',
+          actors: '',
+          awards: '',
+          country: '',
+          director: '',
+          genre: '',
+          imdbID: '',
+          imdbRating: '',
+          language: '',
+          metascore: '',
+          plot: '',
+          poster: 'http://images.com/picture01.jpg',
+          production: 'movie company ltd',
+          rating: 4,
+          ratings: [],
+          released: '2019',
+          runtime: '120 min',
+          title: 'the movie',
+          website: '',
+          writer: '',
+          year: '2019',
+        },
+      },
+    }
+    store = mockStore(state)
+    props = {}
+  })
+
+  it('renders without movie', () => {
+    state = {
+      search: {
+        loading: false,
+        movie: null,
+      },
+    }
+    store = mockStore(state)
+    utils = render(
+      <Root store={store}>
+        <Router>
+          <Theme>
+            <RateNewMovie {...props} />
+          </Theme>
+        </Router>
+      </Root>,
+    )
+
+    const searchText = utils.getByText(/Search for a title/i)
+    expect(searchText).toBeDefined()
+  })
+
+  it('renders with movie', () => {
+    utils = render(
+      <Root store={store}>
+        <Router>
+          <Theme>
+            <RateNewMovie {...props} />
+          </Theme>
+        </Router>
+      </Root>,
+    )
+
+    expect(utils.getByText(/Search result/i)).toBeDefined()
+    expect(utils.getByText(/More details/i)).toBeDefined()
+    expect(utils.getByText(state.search.movie.title)).toBeDefined()
+  })
+})
