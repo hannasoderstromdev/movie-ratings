5c2b72af6b025d32e6212c3b5b468413945f8bb0
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-02 10:47:42 +0100

    test: for reducer

---
+++ client/src/reducers/errorHandler/__tests__/errorHandler.reducer.spec.js
@@ -0,0 +1,42 @@
+import errorHandlerTypes from 'actions/errorHandler/errorHandler.types'
+import reducer from '../errorHandler.reducer'
+
+describe('Reducers/ErrorHandler', () => {
+  const initialState = {
+    error: false,
+    type: null,
+    message: '',
+    status: 0,
+  }
+
+  it(`handles ${errorHandlerTypes.SET_ERROR}`, () => {
+    const type = 'danger'
+    const message = 'Unauthorized'
+    const status = 403
+    const expectedState = {
+      error: true,
+      type,
+      message,
+      status,
+    }
+    const action = {
+      type: errorHandlerTypes.SET_ERROR,
+      payload: { type, message, status },
+    }
+    expect(reducer(initialState, action)).toEqual(expectedState)
+  })
+
+  it(`handles ${errorHandlerTypes.CLEAR_ERROR}`, () => {
+    const state = {
+      error: true,
+      type: 'danger',
+      message: 'Unauthorized',
+      status: 403,
+
+    }
+    const action = {
+      type: errorHandlerTypes.CLEAR_ERROR,
+    }
+    expect(reducer(state, action)).toEqual(initialState)
+  })
+})
