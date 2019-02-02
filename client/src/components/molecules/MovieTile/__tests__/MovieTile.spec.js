9f2a148e06bc1eb9c0e51dacb831a3ecc6d23fd1
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-02 11:23:22 +0100

    test: for MovieTile

---
+++ client/src/components/molecules/MovieTile/__tests__/MovieTile.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+
+import MovieTile from '..'
+
+describe('Component/Molecules/MovieTile', () => {
+  it('renders', () => {
+    const props = {
+      poster: 'http://images.com/picture.jpg',
+      rating: 4,
+      openFullMovie: jest.fn(),
+    }
+    const { getByTestId } = render(<MovieTile {...props} />)
+    expect(getByTestId('movie-tile')).toBeDefined()
+    expect(getByTestId('rating').children.length).toEqual(5)
+  })
+})
