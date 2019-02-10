b8cdfcb45350286304bb54d3311448243a441bc5
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-10 15:22:56 +0100

    feat: add searchById and rename searchByTitle

--- client/src/actions/search/search.actions.js
+++ client/src/actions/search/search.actions.js
@@ -1,22 +1,38 @@
 import searchTypes from './search.types'
 
-const searchForMovieTitle = () => ({
+const searchByTitle = () => ({
   type: searchTypes.SEARCH_FOR_MOVIE_TITLE,
 })
 
-const searchForMovieTitleSuccess = (movie, inLibrary) => ({
+const searchByTitleSuccess = (movie, inLibrary) => ({
   type: searchTypes.SEARCH_FOR_MOVIE_TITLE_SUCCESS,
   payload: { movie, inLibrary },
 })
 
-const searchForMovieTitleFailure = () => ({
+const searchByTitleFailure = () => ({
   type: searchTypes.SEARCH_FOR_MOVIE_TITLE_FAILURE,
 })
 
+const searchById = () => ({
+  type: searchTypes.SEARCH_FOR_MOVIE_ID,
+})
+
+const searchByIdSuccess = (movie, inLibrary) => ({
+  type: searchTypes.SEARCH_FOR_MOVIE_ID_SUCCESS,
+  payload: { movie, inLibrary },
+})
+
+const searchByIdFailure = () => ({
+  type: searchTypes.SEARCH_FOR_MOVIE_ID_FAILURE,
+})
+
 const searchActions = {
-  searchForMovieTitle,
-  searchForMovieTitleSuccess,
-  searchForMovieTitleFailure,
+  searchByTitle,
+  searchByTitleSuccess,
+  searchByTitleFailure,
+  searchById,
+  searchByIdSuccess,
+  searchByIdFailure,
 }
 
 export default searchActions
