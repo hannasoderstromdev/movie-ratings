870fbbefdf0c24bf587fc04061239dfa876c93a9
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-10 17:25:21 +0100

    refactor: using request-promise-native and handle query imdbId

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const request = require('request-promise-native')
 const keys = require('../config/keys')
 
 module.exports = {
@@ -7,46 +7,51 @@ module.exports = {
    */
 
   // Sets res.locals and then defers to movie.controller.findExisting
-  search: function(req, res, next) {
-    const response = request(
-      `http://www.omdbapi.com/?apikey=${keys.OMDB_API_KEY}&t=${
-        req.query.movieTitle
-      }`,
-      function(error, response, body) {
-        if (error) {
-          next(error)
-        } else {
-          const data = JSON.parse(response.body)
+  search: async (req, res, next) => {
+    try {
+      let response
 
-          res.locals = {
-            status: 'success',
-            message: 'movie found',
-            data: {
-              title: data.Title,
-              year: data.Year,
-              released: data.Released,
-              runtime: data.Runtime,
-              genre: data.Genre,
-              director: data.Director,
-              writer: data.Writer,
-              actors: data.Actors,
-              plot: data.Plot,
-              language: data.Language,
-              country: data.Country,
-              awards: data.Awards,
-              poster: data.Poster,
-              ratings: data.Ratings,
-              metascore: data.Metascore,
-              imdbRating: data.imdbRating,
-              imdbVotes: data.imdbVotes,
-              imdbID: data.imdbID,
-              production: data.Production,
-              website: data.Website,
-            },
-          }
-          next()
-        }
-      },
-    )
+      if (req.query.movieTitle) {
+        response = await request(
+          `http://www.omdbapi.com/?apikey=${keys.OMDB_API_KEY}&t=${req.query.movieTitle}`,
+        )
+      } else if (req.query.imdbId) {
+        response = await request(
+          `http://www.omdbapi.com/?apikey=${keys.OMDB_API_KEY}&i=${req.query.imdbId}`,
+        )
+      }
+
+      const data = JSON.parse(response)
+
+      res.locals = {
+        status: 'success',
+        message: 'movie found',
+        data: {
+          title: data.Title,
+          year: data.Year,
+          released: data.Released,
+          runtime: data.Runtime,
+          genre: data.Genre,
+          director: data.Director,
+          writer: data.Writer,
+          actors: data.Actors,
+          plot: data.Plot,
+          language: data.Language,
+          country: data.Country,
+          awards: data.Awards,
+          poster: data.Poster,
+          ratings: data.Ratings,
+          metascore: data.Metascore,
+          imdbRating: data.imdbRating,
+          imdbVotes: data.imdbVotes,
+          imdbID: data.imdbID,
+          production: data.Production,
+          website: data.Website,
+        },
+      }
+      next()
+    } catch (error) {
+      next(error)
+    }
   },
 }
