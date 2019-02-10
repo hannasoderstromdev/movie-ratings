837f7da253a5fa3e89b13cda58c0155b1180c90b
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-10 16:03:21 +0100

    test: refactor test

--- client/src/components/atoms/InputField/__tests__/InputField.spec.js
+++ client/src/components/atoms/InputField/__tests__/InputField.spec.js
@@ -1,42 +1,39 @@
 import React from 'react'
-import { render, cleanup, fireEvent } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 
 import InputField from '..'
 import Theme from '../../../Theme'
 
 describe('<InputField />', () => {
   let onChange
+  let onBlur
   let props
+  let utils
 
   beforeEach(() => {
     onChange = jest.fn()
+    onBlur = jest.fn()
     props = {
       name: 'name',
       type: 'text',
       placeholder: 'placeholder',
       value: '',
       onChange,
+      onBlur,
     }
-  })
-
-  afterEach(cleanup)
-
-  it('renders', () => {
-    const { getByTestId } = render(
+    utils = render(
       <Theme>
         <InputField {...props} />
       </Theme>,
     )
-    expect(getByTestId('input').tagName).toBe('INPUT')
+  })
+
+  it('renders', () => {
+    expect(utils.getByTestId('input').tagName).toBe('INPUT')
   })
 
   it('handles input', () => {
-    const { getByTestId } = render(
-      <Theme>
-        <InputField {...props} />
-      </Theme>,
-    )
-    fireEvent.change(getByTestId('input'), { target: { value: 'A' } })
+    fireEvent.change(utils.getByTestId('input'), { target: { value: 'A' } })
     expect(onChange).toHaveBeenCalledTimes(1)
   })
 })
