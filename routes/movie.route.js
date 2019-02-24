478b19938d399f68b104b8f1f99deaaae8e8f0a0
Hanna Söderström <gothbarbie84@gmail.com>
2019-02-24 09:57:07 +0100

    feat: add role protection middleware for relevant routes

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -2,6 +2,9 @@ const { Router } = require('express')
 const router = new Router()
 
 const movieController = require('../controllers/movie.controller')
+const userController = require('../controllers/user.controller')
+
+const { userIsAdmin } = require('../middlewares/userIsAdmin')
 
 router.get('/', movieController.getAll)
 
@@ -13,12 +16,22 @@ router.get('/rating/:rating', movieController.getByRating)
 
 router.get('/random/:amount', movieController.getRandom)
 
-router.post('/create', movieController.validate('create'), movieController.create)
+router.post('/create', userIsAdmin, movieController.validate('create'), movieController.create)
 
 router.get('/:movieId(d+)', movieController.validate('getById'), movieController.getById)
 
-router.put('/:movieId', movieController.validate('updateById'), movieController.updateById)
+router.put(
+  '/:movieId',
+  userIsAdmin,
+  movieController.validate('updateById'),
+  movieController.updateById,
+)
 
-router.delete('/:movieId', movieController.validate('deleteById'), movieController.deleteById)
+router.delete(
+  '/:movieId',
+  userIsAdmin,
+  movieController.validate('deleteById'),
+  movieController.deleteById,
+)
 
 module.exports = router
