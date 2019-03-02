76cf4d3761c6df8a0b82996572427f1d04e9a432
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-02 16:34:14 +0100

    chore: remove console.log

--- middlewares/userIsAdmin.js
+++ middlewares/userIsAdmin.js
@@ -1,7 +1,6 @@
 module.exports = {
   userIsAdmin: (req, res, next) => {
     try {
-      console.log('res', res.locals.user.role)
       if (res.locals.user.role !== 'Admin') {
         res.status(401).json({ status: 'error', message: 'Unauthorized', data: null })
       } else {
