8bbfffa13831c7df98d51328b3b1bbbe210d3f5a
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-02 16:31:02 +0100

    feat: new component

---
+++ client/src/components/molecules/Genres/Genres.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { Text } from 'components/atoms/Typography'
+
+const Wrapper = styled.div`
+  margin-top: 0.5rem;
+
+  span {
+    margin-right: 0.5rem;
+  }
+`
+
+const Genres = ({ genres }) => (
+  <Wrapper>
+    {genres &&
+      genres.length &&
+      genres.map((genre, i) => <Text key={i}>{genre}</Text>)}
+  </Wrapper>
+)
+
+Genres.propTypes = {
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Genres
