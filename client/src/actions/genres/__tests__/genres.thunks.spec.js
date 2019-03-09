f64e35b1577d0937b3bc632b974c40386124a66b
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-09 16:24:31 +0100

    test: for genres thunks

---
+++ client/src/actions/genres/__tests__/genres.thunks.spec.js
@@ -0,0 +1,69 @@
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
+
+import genresThunks from '../genres.thunks'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+describe('Actions/Genres/Thunks', () => {
+  let initialState
+  let store
+  fetchMock.config.overwriteRoutes = true
+
+  beforeEach(() => {
+    initialState = {}
+    store = mockStore(initialState)
+    localStorage.setItem('user', JSON.stringify({ token: 'fake token' }))
+  })
+
+  afterEach(() => {
+    localStorage.clear()
+  })
+
+  describe('getAll', () => {
+    it('dispatches the correct actions', async () => {
+      fetchMock.mock('/genres/', {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: {
+          message: 'success',
+          data: {
+            genres: [
+              {
+                _id: 'genreId01',
+                name: 'Drama',
+              },
+              {
+                _id: 'genreId02',
+                name: 'Action',
+              },
+            ],
+          },
+        },
+      })
+
+      await store.dispatch(genresThunks.getAll())
+      const actions = store.getActions()
+      const expected = [
+        { type: 'LOAD_GENRES_INIT' },
+        {
+          payload: {
+            genres: {
+              genres: [
+                { _id: 'genreId01', name: 'Drama' },
+                { _id: 'genreId02', name: 'Action' },
+              ],
+            },
+          },
+          type: 'LOAD_GENRES_SUCCESS',
+        },
+      ]
+
+      expect(actions).toEqual(expected)
+    })
+  })
+})
