f86d64122dde62a941fde89d2c25d2ef742834d0
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-09 16:31:04 +0100

    feat: new reducer

--- client/src/reducers/genres/genres.reducer.js
+++ client/src/reducers/genres/genres.reducer.js
@@ -1,11 +1,33 @@
 import genresTypes from 'actions/genres/genres.types'
 
-const initialState = {}
+const initialState = {
+  loading: false,
+  genres: [],
+  error: false,
+}
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case genresTypes.LOAD_GENRES:
-      return action.payload.genres
+    case genresTypes.LOAD_GENRES_INIT:
+      return {
+        ...state,
+        loading: true,
+      }
+
+    case genresTypes.LOAD_GENRES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        genres: action.payload.genres,
+        error: false,
+      }
+
+    case genresTypes.LOAD_GENRES_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      }
 
     default:
       return state
