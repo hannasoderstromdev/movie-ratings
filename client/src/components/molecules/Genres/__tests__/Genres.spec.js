378fe41fa0c9c1e33daa8f37d6df629709d5bc84
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-10 09:21:06 +0100

    fix: genres

--- client/src/components/molecules/Genres/__tests__/Genres.spec.js
+++ client/src/components/molecules/Genres/__tests__/Genres.spec.js
@@ -7,7 +7,11 @@ import Genres from '..'
 describe('Components/Molecules/Genres', () => {
   it('renders', () => {
     const props = {
-      genres: ['Drama', 'Adventure', 'Sci-Fi'],
+      genres: [
+        { _id: 'genreId01', name: 'Drama' },
+        { _id: 'genreId02', name: 'Adventure' },
+        { _id: 'genreId03', name: 'Sci-Fi' },
+      ],
     }
     const utils = render(
       <Theme>
@@ -15,8 +19,8 @@ describe('Components/Molecules/Genres', () => {
       </Theme>,
     )
 
-    expect(utils.getByText(props.genres[0])).toBeDefined()
-    expect(utils.getByText(props.genres[1])).toBeDefined()
-    expect(utils.getByText(props.genres[2])).toBeDefined()
+    expect(utils.getByText(props.genres[0].name)).toBeDefined()
+    expect(utils.getByText(props.genres[1].name)).toBeDefined()
+    expect(utils.getByText(props.genres[2].name)).toBeDefined()
   })
 })
