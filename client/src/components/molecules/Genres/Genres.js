465cf1f9abfd601166b551f8aacd6bb0d9a095e1
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-10 12:33:28 +0100

    refactor: make genre separate component

--- client/src/components/molecules/Genres/Genres.js
+++ client/src/components/molecules/Genres/Genres.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import Icon from 'components/atoms/Icon'
+import Genre from 'components/molecules/Genre'
 
 const Wrapper = styled.div`
   margin-top: 1rem;
@@ -14,61 +14,15 @@ const Wrapper = styled.div`
   }
 `
 
-const Genre = styled.button`
-  border-radius: 3px;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  background-color: ${({ theme, selected }) =>
-    selected ? theme.colors.primary : theme.colors.dark};
-  color: ${({ theme, selected }) =>
-    selected ? theme.colors.textDark : theme.colors.primary};
-  font-size: ${({ theme }) => theme.fontSizes.textSmall};
-  outline: none;
-  margin: 0 1rem 1rem 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  svg {
-    margin-left: 0.5rem;
-  }
-`
-
-const Genres = ({ genres, onClick }) => {
-  return onClick ? (
-    <Wrapper>
-      {genres &&
-        !!genres.length &&
-        genres.map(genre => (
-          <Genre
-            key={genre._id}
-            onClick={() => onClick(genre._id)}
-            selected={genre.selected}
-          >
-            {genre.name}
-            {genre.selected ? (
-              <Icon icon={['fas', 'minus']} />
-            ) : (
-              <Icon icon={['fas', 'plus']} />
-            )}
-          </Genre>
-        ))}
-    </Wrapper>
-  ) : (
-    <Wrapper>
-      {genres &&
-        !!genres.length &&
-        genres.map(genre => <Genre key={genre._id}>{genre.name}</Genre>)}
-    </Wrapper>
-  )
-}
-
-Genres.defaultProps = {
-  onClick: undefined,
-}
+const Genres = ({ genres }) => (
+  <Wrapper>
+    {genres &&
+      !!genres.length &&
+      genres.map(genre => (
+        <Genre id={genre._id} key={genre._id} name={genre.name} />
+      ))}
+  </Wrapper>
+)
 
 Genres.propTypes = {
   genres: PropTypes.arrayOf(
@@ -77,7 +31,6 @@ Genres.propTypes = {
       name: PropTypes.string,
     }),
   ).isRequired,
-  onClick: PropTypes.func,
 }
 
 export default Genres
