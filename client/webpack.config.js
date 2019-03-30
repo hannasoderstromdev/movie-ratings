8c31642ca4384e2f02f4ffd57b81ea5f30e79e0d
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-30 15:32:03 +0100

    feat: add hot module replacement

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
 const keys = require('../config/keys')
 
 module.exports = {
   devServer: {
+    contentBase: path.resolve(__dirname, 'dist/static/'),
     proxy: {
       '/users/authenticate': keys.ROOT_URL,
       '/search': keys.ROOT_URL,
@@ -12,7 +15,9 @@ module.exports = {
       '/movies/latest': keys.ROOT_URL,
       '/genres': keys.ROOT_URL,
     },
+    hot: true,
   },
+  devtool: 'inline-source-map',
   resolve: {
     modules: [path.resolve(__dirname, './src'), 'node_modules'],
     extensions: ['.js', '.jsx', '.json'],
@@ -21,10 +26,10 @@ module.exports = {
     },
   },
   entry: {
-    path: path.resolve(__dirname, 'src/index.js'),
+    app: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist/static/'),
+    path: path.resolve(__dirname, 'build/static/'),
     filename: '[name].bundle.js',
   },
   module: {
@@ -65,5 +70,6 @@ module.exports = {
       template: './build/index.html',
       filename: './index.html',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 }
