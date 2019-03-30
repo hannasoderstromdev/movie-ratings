19d036a31837e561b18c08bae7d0597e54dd3fc4
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-30 13:56:37 +0100

    feat: add moduleNameMapper for images etc

--- client/jest.config.js
+++ client/jest.config.js
@@ -62,10 +62,10 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx'],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {
-  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-  //     '<rootDir>/tests/__mocks__/fileMock.js',
-  // },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>src/tests/__mocks__/fileMock.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -153,11 +153,11 @@ module.exports = {
   timers: 'fake',
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>src/fileTransformer.js',
-    '^.+\\.js$': 'babel-jest',
-  },
+  // transform: {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+  //     '<rootDir>src/tests/fileTransformer.js',
+  //   '^.+\\.js$': 'babel-jest',
+  // },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['/node_modules/'],
