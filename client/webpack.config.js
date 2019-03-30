5bf1471512a2352d8ef562d475f5892f007217ef
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-30 15:58:21 +0100

    feat: setup production build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const keys = require('../config/keys')
 
-module.exports = {
+module.exports = env => ({
   devServer: {
     contentBase: path.resolve(__dirname, 'dist/static/'),
     proxy: {
@@ -15,9 +16,61 @@ module.exports = {
       '/movies/latest': keys.ROOT_URL,
       '/genres': keys.ROOT_URL,
     },
-    hot: true,
+    hot: env === 'development',
   },
-  devtool: 'inline-source-map',
+  devtool: env === 'development' ? 'inline-source-map' : false,
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename:
+      env === 'development'
+        ? 'static/js/bundle.js'
+        : 'static/js/[name].[hash:8].js',
+    chunkFilename:
+      env === 'development'
+        ? 'static/js/[name].chunk.js'
+        : 'static/js/[name].[hash:8].chunk.js',
+    devtoolModuleFilenameTemplate:
+      env === 'development'
+        ? info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
+        : info =>
+            path
+              .relative(path.appSrc, info.absoluteResourcePath)
+              .replace(/\\/g, '/'),
+  },
+  optimization: {
+    minimize: env === 'development',
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: { ecma: 8 },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        },
+        parallel: true,
+        cache: true,
+        sourceMap: env === 'development',
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+    runtimeChunk: true,
+  },
   resolve: {
     modules: [path.resolve(__dirname, './src'), 'node_modules'],
     extensions: ['.js', '.jsx', '.json'],
@@ -25,15 +78,9 @@ module.exports = {
       reducers: path.resolve(__dirname, './src/reducers'),
     },
   },
-  entry: {
-    app: path.resolve(__dirname, 'src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'build/static/'),
-    filename: '[name].bundle.js',
-  },
   module: {
     rules: [
+      { parser: { requireEnsure: false } },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -43,6 +90,7 @@ module.exports = {
       },
       {
         test: /\.(js|jsx)$/,
+        enforce: 'pre',
         exclude: /node_modules/,
         loader: 'eslint-loader',
         options: {
@@ -72,4 +120,4 @@ module.exports = {
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
-}
+})
