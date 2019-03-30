c228430b3ed034060662f696826c75f2b8158016
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-30 14:53:47 +0100

    fix: broken ref to keys

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const keys = require('../../config/keys')
+const keys = require('../config/keys')
 
 module.exports = {
   devServer: {
