ff4b4b3d11c068792909255b9e17ea2a27324fa1
Hanna Söderström <gothbarbie84@gmail.com>
2019-03-31 16:21:58 +0200

    feat: change dir to dist from build

--- index.js
+++ index.js
@@ -44,12 +44,12 @@ app.use('/genres', validateUser, genre)
  */
 if (process.env.NODE_ENV === 'production') {
   // First hand: Serve specific file
-  app.use(express.static('client/build'))
+  app.use(express.static('client/dist'))
 
   // Fallback: Serve index.html
   const path = require('path')
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   })
 }
 
