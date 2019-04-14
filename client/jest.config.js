cf46dde6dd87e509b1be3af82e919ae75df81f39
Hanna Söderström <gothbarbie84@gmail.com>
2019-04-14 14:13:25 +0200

    fix: remove file transform

--- client/jest.config.js
+++ client/jest.config.js
@@ -62,10 +62,10 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx'],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>src/tests/__mocks__/fileMock.js',
-  },
+  // moduleNameMapper: {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+  //     '<rootDir>src/tests/__mocks__/fileMock.js',
+  // },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
