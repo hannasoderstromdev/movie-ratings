aa5fe847dd9a336d42b2aa341bff0eb3ceeba6f1
Hanna Söderström <gothbarbie84@gmail.com>
2019-04-28 10:30:03 +0200

    refactor: icons to separate file

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,31 +1,4 @@
 import 'jest-styled-components'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faCheckCircle,
-  faChevronDown,
-  faChevronUp,
-  faChevronLeft,
-  faChevronRight,
-  faCog,
-  faEllipsisV,
-  faEllipsisH,
-  faExclamationCircle,
-  faExclamationTriangle,
-  faMinus,
-  faPlus,
-  faPlusCircle,
-  faSearch,
-  faSearchPlus,
-  faStar,
-  faTimes,
-  faTrashAlt,
-  faList,
-  faLock,
-  faUnlock,
-  faTh,
-  faThList,
-} from '@fortawesome/free-solid-svg-icons'
 import 'jest-localstorage-mock'
 
 // Adds additional expect-functions
@@ -35,28 +8,4 @@ import 'jest-axe/extend-expect'
 // Cleans up DOM after each test automatically
 import 'react-testing-library/cleanup-after-each'
 
-library.add(
-  faCheckCircle,
-  faChevronDown,
-  faChevronUp,
-  faChevronLeft,
-  faChevronRight,
-  faCog,
-  faEllipsisV,
-  faEllipsisH,
-  faExclamationCircle,
-  faExclamationTriangle,
-  faMinus,
-  faPlus,
-  faPlusCircle,
-  faSearch,
-  faSearchPlus,
-  faStar,
-  faTimes,
-  faTrashAlt,
-  faList,
-  faLock,
-  faUnlock,
-  faTh,
-  faThList,
-)
+import './fontAwesome'
