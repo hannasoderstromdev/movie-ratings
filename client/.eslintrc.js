62a209f8f6dc53d96aed48a11af69a364e6ba455
Hanna Söderström <gothbarbie84@gmail.com>
2019-05-01 10:59:24 +0200

    feat: update rule for any

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -47,6 +47,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     eqeqeq: ["error", "always" ],
