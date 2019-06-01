cc41c12b7fc5bfa0994032b6680fded5850145af
Hanna Söderström <gothbarbie84@gmail.com>
2019-06-01 12:49:10 +0200

    Update deps (#14)
    * feat: update dependencies
    
    * fix: CleanWebpackPlugin import

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const TerserPlugin = require('terser-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
